0685da6565685f3df7cd76a16a1ad3bb4cc1fb79
jyeost <pig9619@naver.com>
2023-07-12 21:15:01 +0900

    feat: Post 클래스 구현
    Co-authored-by: hectick <ch.yeondev@gmail.com>
    Co-authored-by: this2sho <zx00018@naver.com>
    Co-authored-by: backfoxx <backfoxxx@gmail.com>

---
+++ backend/src/main/java/edonymyeon/backend/domain/Post.java
@@ -0,0 +1,47 @@
+package edonymyeon.backend.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@DynamicInsert
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "longtext")
+    private String content;
+
+    // TODO: 작성자
+    // TODO: 사진들
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+
+    @ColumnDefault("0")
+    private Long viewCount;
+}
