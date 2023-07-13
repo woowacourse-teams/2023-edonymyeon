30e902114cef6e31e58770a993fd06544f390641
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-07-13 19:56:08 +0900

    feat: 로그인 뷰 구현
    포인트 컬러 배경 제작
    dohyeon.ttf 추가

---
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.app.edonymyeon.presentation.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.edonymyeon.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
