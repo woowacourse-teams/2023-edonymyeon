49df8c56886a8afaecc31cf3c0dfc54404b9f920
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-07-14 16:47:27 +0900

    feat: 게시글 작성/수정 페이지 뷰 구현

---
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/posteditor/PostEditorActivity.kt
@@ -0,0 +1,12 @@
+package com.app.edonymyeon.presentation.ui.posteditor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.edonymyeon.R
+
+class PostEditorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_editor)
+    }
+}
