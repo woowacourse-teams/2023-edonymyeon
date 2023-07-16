1cdbea0ae4374f830c53e6330b3404a77daf3055
BackFoxx <backfoxxx@gmail.com>
2023-07-16 15:58:49 +0900

    refactor: 코딩 컨벤션에 맞게 코드 스타일 수정

--- backend/src/main/java/edonymyeon/backend/service/request/PostRequest.java
+++ backend/src/main/java/edonymyeon/backend/service/request/PostRequest.java
@@ -3,8 +3,12 @@ package edonymyeon.backend.service.request;
 import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
-public record PostRequest(String title, String content, Long price, List<MultipartFile> images) {
-
+public record PostRequest(
+        String title,
+        String content,
+        Long price,
+        List<MultipartFile> images
+) {
 
 }
 
