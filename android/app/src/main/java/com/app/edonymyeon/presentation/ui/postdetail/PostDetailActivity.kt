715d196127250aa07942cde718d148df65fc0709
Sujin Kim <sujinkim919@gmail.com>
2023-07-16 17:14:08 +0900

    feat: 게시글 상세 페이지 상단 앱바 구현

--- android/app/src/main/java/com/app/edonymyeon/presentation/ui/postdetail/PostDetailActivity.kt
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/postdetail/PostDetailActivity.kt
@@ -1,12 +1,59 @@
 package com.app.edonymyeon.presentation.ui.postdetail
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import app.edonymyeon.R
+import app.edonymyeon.databinding.ActivityPostDetailBinding
+import com.google.android.material.snackbar.Snackbar
 
 class PostDetailActivity : AppCompatActivity() {
+    private val binding: ActivityPostDetailBinding by lazy {
+        ActivityPostDetailBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_detail)
+        setContentView(binding.root)
+
+        initAppbar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_post_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_scrap -> {
+                item.isChecked = !item.isChecked
+                if (item.isChecked) {
+                    item.setIcon(R.drawable.btn_scrap_selected)
+                } else {
+                    item.setIcon(R.drawable.btn_scrap_unselected)
+                }
+                return true
+            }
+
+            R.id.action_update -> {
+                Snackbar.make(binding.root, "update", Snackbar.LENGTH_SHORT).show()
+                return true
+            }
+
+            R.id.action_delete -> {
+                Snackbar.make(binding.root, "delete", Snackbar.LENGTH_SHORT).show()
+                return true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun initAppbar() {
+        setSupportActionBar(binding.tbPostDetail)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
     }
 }
