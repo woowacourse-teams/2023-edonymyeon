320ad1a5adabc20bc216ae1ac2d43cc47182a528
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-07-16 16:59:56 +0900

    feat: bottom navigation을 fragment와 연결

---
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/main/alarm/AlarmFragment.kt
@@ -0,0 +1,18 @@
+package com.app.edonymyeon.presentation.ui.main.alarm
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import app.edonymyeon.R
+
+class AlarmFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_preparing_page, container, false)
+    }
+}
---
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/main/home/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.app.edonymyeon.presentation.ui.main.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import app.edonymyeon.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
