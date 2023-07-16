7d3b87008e243cce9a5be95e7a80a723b9687615
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-07-17 01:49:15 +0900

    feat:  제목, 가격, 게시글 내용을 입력받는 기능 구현

--- android/app/src/main/java/com/app/edonymyeon/presentation/ui/posteditor/PostEditorViewModel.kt
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/posteditor/PostEditorViewModel.kt
@@ -1,6 +1,7 @@
 package com.app.edonymyeon.presentation.ui.posteditor
 
 import android.app.Application
+import android.text.Editable
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -8,8 +9,20 @@ import androidx.lifecycle.MutableLiveData
 class PostEditorViewModel(application: Application) : AndroidViewModel(application) {
     private val images = mutableListOf<String>()
     private val _galleryImages = MutableLiveData<List<String>>()
+    private val _postTitle = MutableLiveData<String>()
+    private val _postPrice = MutableLiveData<String>()
+    private val _postContent = MutableLiveData<String>()
     val galleryImages: LiveData<List<String>>
         get() = _galleryImages
+
+    val postTitle: LiveData<String>
+        get() = _postTitle
+
+    val postPrice: LiveData<String>
+        get() = _postPrice
+
+    val postContent: LiveData<String>
+        get() = _postContent
 
     init {
         _galleryImages.value = images
@@ -23,5 +36,17 @@ class PostEditorViewModel(application: Application) : AndroidViewModel(applicati
     fun deleteSelectedImages(image: String) {
         images.remove(image)
         _galleryImages.value = images
+    }
+
+    fun setTitle(editTitle: Editable) {
+        _postTitle.value = editTitle.toString()
+    }
+
+    fun setPrice(editPrice: Editable) {
+        _postPrice.value = editPrice.toString()
+    }
+
+    fun setContent(editContent: Editable) {
+        _postContent.value = editContent.toString()
     }
 }
