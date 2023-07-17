1067f72b5fb807c85fe2f947618357bee7b375f4
This2sho <zx00018@naver.com>
2023-07-17 21:54:02 +0900

    feat: 회원 프로필 사진 클래스 생성

--- backend/src/main/java/edonymyeon/backend/domain/Member.java
+++ backend/src/main/java/edonymyeon/backend/domain/Member.java
@@ -2,9 +2,12 @@ package edonymyeon.backend.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,6 +32,8 @@ public class Member {
 
     private String introduction;
 
-    //todo: 프로필 사진
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private ProfileImageInfo profileImageInfo;
     //todo: 검증 로직
 }
