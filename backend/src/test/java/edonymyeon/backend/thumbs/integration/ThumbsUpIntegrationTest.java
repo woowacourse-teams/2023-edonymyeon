7a079a2ed0e9523b01678a2b1aa03d08a1192fc3
jyeost <pig9619@naver.com>
2023-07-18 22:00:42 +0900

    refactor: 테스트 클래스 어노테이션 추가

--- backend/src/test/java/edonymyeon/backend/thumbs/integration/ThumbsUpIntegrationTest.java
+++ backend/src/test/java/edonymyeon/backend/thumbs/integration/ThumbsUpIntegrationTest.java
@@ -18,14 +18,14 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.MethodMode;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestConstructor.AutowireMode;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @TestConstructor(autowireMode = AutowireMode.ALL)
-@DirtiesContext(methodMode = MethodMode.AFTER_METHOD)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ThumbsUpIntegrationTest {
 
