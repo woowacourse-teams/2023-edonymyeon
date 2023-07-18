31a29a91dfe2ecd13afbe67cc1d5f898ce82b300
hectick <ch.yeondev@gmail.com>
2023-07-18 17:24:25 +0900

    feat: Member 클래스에 AllArgsConstructor 어노테이션 추가

--- backend/src/main/java/edonymyeon/backend/member/domain/Member.java
+++ backend/src/main/java/edonymyeon/backend/member/domain/Member.java
@@ -10,10 +10,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Member {
