44ceac1a5ee6e53c2996e1e82c83b497052a616e
hectick <ch.yeondev@gmail.com>
2023-07-18 21:27:10 +0900

    test: PostController 테스트 리팩토링

--- backend/src/test/java/edonymyeon/backend/controller/PostControllerTest.java
+++ backend/src/test/java/edonymyeon/backend/controller/PostControllerTest.java
@@ -1,12 +1,14 @@
 package edonymyeon.backend.controller;
 
 import static edonymyeon.backend.global.exception.ExceptionInformation.POST_MEMBER_FORBIDDEN;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edonymyeon.backend.global.controlleradvice.dto.ExceptionResponse;
 import edonymyeon.backend.post.application.dto.PostResponse;
-import java.io.InputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -27,6 +29,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 @SuppressWarnings("NonAsciiCharacters")
+@Sql("/dummydata.sql")
 @Transactional
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SpringBootTest
@@ -36,22 +39,33 @@ class PostControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    private static MockMultipartFile 이미지1;
+
+    private static MockMultipartFile 이미지2;
+
+    static {
+        try {
+            이미지1 = new MockMultipartFile(
+                    "images",
+                    "test_image_1.jpg",
+                    "image/jpg",
+                    PostControllerTest.class.getResourceAsStream("/static/img/file/test_image_1.jpg")
+            );
+            이미지2 = new MockMultipartFile(
+                    "images",
+                    "test_image_2.jpg",
+                    "image/jpg",
+                    PostControllerTest.class.getResourceAsStream("/static/img/file/test_image_2.jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Test
-    @Sql("/dummydata.sql")
     void 사진_첨부_성공_테스트() throws Exception {
-        // 파일 1
-        InputStream file1InputStream = getClass().getResourceAsStream("/static/img/file/test_image_1.jpg");
-        MockMultipartFile file1 = new MockMultipartFile("images", "test_image_1.jpg", "image/jpg",
-                file1InputStream);
-
-        // 파일 2
-        InputStream file2InputStream = getClass().getResourceAsStream("/static/img/file/test_image_2.jpg");
-        MockMultipartFile file2 = new MockMultipartFile("images", "test_image_2.jpg", "image/jpg",
-                file2InputStream);
-
         mockMvc.perform(MockMvcRequestBuilders.multipart("/posts")
-                        .file(file1)
-                        .file(file2)
+                        .file(이미지1)
+                        .file(이미지2)
                         .param("title", "test title")
                         .param("content", "test content")
                         .param("price", "1000")
@@ -63,21 +77,10 @@ class PostControllerTest {
     }
 
     @Test
-    @Sql("/dummydata.sql")
     void 회원이_아니면_게시글_작성_불가_테스트() throws Exception {
-        // 파일 1
-        InputStream file1InputStream = getClass().getResourceAsStream("/static/img/file/test_image_1.jpg");
-        MockMultipartFile file1 = new MockMultipartFile("images", "test_image_1.jpg", "image/jpg",
-                file1InputStream);
-
-        // 파일 2
-        InputStream file2InputStream = getClass().getResourceAsStream("/static/img/file/test_image_2.jpg");
-        MockMultipartFile file2 = new MockMultipartFile("images", "test_image_2.jpg", "image/jpg",
-                file2InputStream);
-
         mockMvc.perform(MockMvcRequestBuilders.multipart("/posts")
-                        .file(file1)
-                        .file(file2)
+                        .file(이미지1)
+                        .file(이미지2)
                         .param("title", "test title")
                         .param("content", "test content")
                         .param("price", "1000")
@@ -87,21 +90,10 @@ class PostControllerTest {
     }
 
     @Test
-    @Sql("/dummydata.sql")
-    void 본인이_작성한_게시글_삭제_테스트() throws Exception {
-        // 파일 1
-        InputStream file1InputStream = getClass().getResourceAsStream("/static/img/file/test_image_1.jpg");
-        MockMultipartFile file1 = new MockMultipartFile("images", "test_image_1.jpg", "image/jpg",
-                file1InputStream);
-
-        // 파일 2
-        InputStream file2InputStream = getClass().getResourceAsStream("/static/img/file/test_image_2.jpg");
-        MockMultipartFile file2 = new MockMultipartFile("images", "test_image_2.jpg", "image/jpg",
-                file2InputStream);
-
-        final MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart("/posts")
-                        .file(file1)
-                        .file(file2)
+    void 본인이_작성한_게시글_삭제_가능_테스트() throws Exception {
+        final MvcResult 게시글_생성_요청_결과 = mockMvc.perform(MockMvcRequestBuilders.multipart("/posts")
+                        .file(이미지1)
+                        .file(이미지2)
                         .param("title", "test title")
                         .param("content", "test content")
                         .param("price", "1000")
@@ -111,33 +103,27 @@ class PostControllerTest {
                 )
                 .andExpect(MockMvcResultMatchers.status().isCreated()).andReturn();
 
-        MockHttpServletResponse mockHttpServletResponse = result.getResponse();
-        String responseBody = mockHttpServletResponse.getContentAsString();
-        ObjectMapper objectMapper = new ObjectMapper();
-        PostResponse response = objectMapper.readValue(responseBody, PostResponse.class);
+        PostResponse 게시글_생성_응답 = extractResponseFromResult(게시글_생성_요청_결과, PostResponse.class);
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/posts/" + response.id())
+        mockMvc.perform(MockMvcRequestBuilders.delete("/posts/" + 게시글_생성_응답.id())
                         .header(HttpHeaders.AUTHORIZATION, "Basic "
                                 + Base64.getEncoder().encodeToString("email:password".getBytes())))
                 .andExpect(MockMvcResultMatchers.status().isNoContent());
     }
 
+    private <T> T extractResponseFromResult(MvcResult mvcResult, Class<T> valueType)
+            throws UnsupportedEncodingException, JsonProcessingException {
+        MockHttpServletResponse mockHttpServletResponse = mvcResult.getResponse();
+        String responseBody = mockHttpServletResponse.getContentAsString(StandardCharsets.UTF_8);
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(responseBody, valueType);
+    }
+
     @Test
-    @Sql("/dummydata.sql")
-    void 본인이_작성하지_않은_게시글_삭제_테스트() throws Exception {
-        // 파일 1
-        InputStream file1InputStream = getClass().getResourceAsStream("/static/img/file/test_image_1.jpg");
-        MockMultipartFile file1 = new MockMultipartFile("images", "test_image_1.jpg", "image/jpg",
-                file1InputStream);
-
-        // 파일 2
-        InputStream file2InputStream = getClass().getResourceAsStream("/static/img/file/test_image_2.jpg");
-        MockMultipartFile file2 = new MockMultipartFile("images", "test_image_2.jpg", "image/jpg",
-                file2InputStream);
-
-        final MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart("/posts")
-                        .file(file1)
-                        .file(file2)
+    void 본인이_작성하지_않은_게시글_삭제_불가능_테스트() throws Exception {
+        final MvcResult 게시글_생성_요청_결과 = mockMvc.perform(MockMvcRequestBuilders.multipart("/posts")
+                        .file(이미지1)
+                        .file(이미지2)
                         .param("title", "test title")
                         .param("content", "test content")
                         .param("price", "1000")
@@ -147,22 +133,20 @@ class PostControllerTest {
                 )
                 .andExpect(MockMvcResultMatchers.status().isCreated()).andReturn();
 
-        MockHttpServletResponse mockHttpServletResponse = result.getResponse();
-        String responseBody = mockHttpServletResponse.getContentAsString(StandardCharsets.UTF_8);
-        ObjectMapper objectMapper = new ObjectMapper();
-        PostResponse response = objectMapper.readValue(responseBody, PostResponse.class);
+        PostResponse 게시글_생성_응답 = extractResponseFromResult(게시글_생성_요청_결과, PostResponse.class);
 
-        final MvcResult result2 = mockMvc.perform(MockMvcRequestBuilders.delete("/posts/" + response.id())
+        final MvcResult 게시글_삭제_요청_결과 = mockMvc.perform(MockMvcRequestBuilders.delete("/posts/" + 게시글_생성_응답.id())
                         .header(HttpHeaders.AUTHORIZATION, "Basic "
                                 + Base64.getEncoder().encodeToString("badman:password".getBytes())))
                 .andExpect(MockMvcResultMatchers.status().isForbidden())
                 .andReturn();
 
-        MockHttpServletResponse mockHttpServletResponse2 = result2.getResponse();
-        String responseBody2 = mockHttpServletResponse2.getContentAsString(StandardCharsets.UTF_8);
-        ExceptionResponse response2 = objectMapper.readValue(responseBody2, ExceptionResponse.class);
+        ExceptionResponse 예외_응답 = extractResponseFromResult(게시글_삭제_요청_결과, ExceptionResponse.class);
 
-        assertThat(response2.errorCode()).isEqualTo(POST_MEMBER_FORBIDDEN.getCode());
-        assertThat(response2.errorMessage()).isEqualTo(POST_MEMBER_FORBIDDEN.getMessage());
+        assertSoftly(softly -> {
+                    softly.assertThat(예외_응답.errorCode()).isEqualTo(POST_MEMBER_FORBIDDEN.getCode());
+                    softly.assertThat(예외_응답.errorCode()).isEqualTo(POST_MEMBER_FORBIDDEN.getCode());
+                }
+        );
     }
 }
