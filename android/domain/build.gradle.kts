1a87530212df5cfde90962112ba56e31ac1fd678
Sujin Kim <sujinkim919@gmail.com>
2023-07-18 17:51:23 +0900

    test: domain test dependency 추가 및 Count test 구현

--- android/domain/build.gradle.kts
+++ android/domain/build.gradle.kts
@@ -7,3 +7,14 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+dependencies {
+    testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
+    testImplementation("org.assertj", "assertj-core", "3.22.0")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
