86f4456ad591bdfca32d3aa6e2b9ecc0aeb80e8d
BackFoxx <backfoxxx@gmail.com>
2023-07-19 15:02:44 +0900

    test: 게시글 전체 조회 상황을 ATDD로 테스트하는 코드 작성

--- backend/src/test/java/edonymyeon/backend/controller/PostControllerTest.java
+++ backend/src/test/java/edonymyeon/backend/controller/PostControllerTest.java
@@ -1,6 +1,13 @@
 package edonymyeon.backend.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.io.InputStream;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
@@ -12,8 +19,6 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 @SuppressWarnings("NonAsciiCharacters")
@@ -22,6 +27,18 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 class PostControllerTest {
+
+    public static final String POST_REQUEST1_TITLE = "Lost in Time";
+    public static final String POST_REQUEST1_CONTENT = "A young archaeologist discovers a mysterious artifact that transports her back in time, forcing her to navigate ancient civilizations and find a way back home before history unravels.";
+    public static final long POST_REQUEST1_PRICE = 14_000L;
+
+    public static final String POST_REQUEST2_TITLE = "Whispers in the Dark";
+    public static final String POST_REQUEST2_CONTENT = "A renowned detective embarks on a perilous journey to solve a series of murders that are linked to a secret society.";
+    public static final long POST_REQUEST2_PRICE = 20_000L;
+
+    public static final String POST_REQUEST3_TITLE = "Silent Shadows";
+    public static final String POST_REQUEST3_CONTENT = "In a desolate town plagued by a decades-old curse, a curious teenager ventures into an abandoned mansion.";
+    public static final long POST_REQUEST3_PRICE = 25_000L;
 
     @Autowired
     MockMvc mockMvc;
@@ -39,7 +56,7 @@ class PostControllerTest {
         MockMultipartFile file2 = new MockMultipartFile("images", "test_image_2.jpg", "image/jpg",
                 file2InputStream);
 
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/posts")
+        mockMvc.perform(multipart("/posts")
                         .file(file1)
                         .file(file2)
                         .param("title", "test title")
@@ -49,7 +66,7 @@ class PostControllerTest {
                         .header(HttpHeaders.AUTHORIZATION, "Basic "
                                 + java.util.Base64.getEncoder().encodeToString("email:password".getBytes()))
                 )
-                .andExpect(MockMvcResultMatchers.status().isCreated());
+                .andExpect(status().isCreated());
     }
 
     @Test
@@ -65,7 +82,7 @@ class PostControllerTest {
         MockMultipartFile file2 = new MockMultipartFile("images", "test_image_2.jpg", "image/jpg",
                 file2InputStream);
 
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/posts")
+        mockMvc.perform(multipart("/posts")
                         .file(file1)
                         .file(file2)
                         .param("title", "test title")
@@ -73,6 +90,127 @@ class PostControllerTest {
                         .param("price", "1000")
                         .contentType(MediaType.MULTIPART_FORM_DATA)
                 )
-                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @Sql("/dummydata.sql")
+    void 전체_게시글을_조회한다() throws Exception {
+        게시글_하나를_등록한다1();
+        게시글_하나를_등록한다2();
+        게시글_하나를_등록한다3();
+
+        mockMvc.perform(get("/posts")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$", Matchers.hasSize(3)));
+    }
+
+    @Test
+    @Sql("/dummydata.sql")
+    void 전체_게시글을_한_페이지당_1개씩_조회한다() throws Exception {
+        게시글_하나를_등록한다1();
+        게시글_하나를_등록한다2();
+        게시글_하나를_등록한다3();
+
+        mockMvc.perform(get("/posts?size=1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$", Matchers.hasSize(1)));
+    }
+
+    @Test
+    @Sql("/dummydata.sql")
+    void 전체_게시글을_한_페이지당_1개씩_2페이지부터_조회한다() throws Exception {
+        게시글_하나를_등록한다1();
+        게시글_하나를_등록한다2();
+        게시글_하나를_등록한다3();
+
+        mockMvc.perform(get("/posts?size=1&page=1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].content", Matchers.equalTo(POST_REQUEST2_CONTENT)));
+    }
+
+    @Test
+    @Sql("/dummydata.sql")
+    void 조회할_게시글_정보가_없다() throws Exception {
+        mockMvc.perform(get("/posts?size=1&page=1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$", Matchers.hasSize(0)));
+    }
+
+    private void 게시글_하나를_등록한다1() throws Exception {
+        // 파일 1
+        InputStream file1InputStream = getClass().getResourceAsStream("/static/img/file/test_image_1.jpg");
+        MockMultipartFile file1 = new MockMultipartFile("images", "test_image_1.jpg", "image/jpg",
+                file1InputStream);
+
+        // 파일 2
+        InputStream file2InputStream = getClass().getResourceAsStream("/static/img/file/test_image_2.jpg");
+        MockMultipartFile file2 = new MockMultipartFile("images", "test_image_2.jpg", "image/jpg",
+                file2InputStream);
+
+        mockMvc.perform(multipart("/posts")
+                        .file(file1)
+                        .file(file2)
+                        .param("title", POST_REQUEST1_TITLE)
+                        .param("content", POST_REQUEST1_CONTENT)
+                        .param("price", String.valueOf(POST_REQUEST1_PRICE))
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                        .header(HttpHeaders.AUTHORIZATION, "Basic "
+                                + java.util.Base64.getEncoder().encodeToString("email:password".getBytes()))
+                )
+                .andExpect(status().isCreated());
+    }
+
+    private void 게시글_하나를_등록한다2() throws Exception {
+        // 파일 1
+        InputStream file1InputStream = getClass().getResourceAsStream("/static/img/file/test_image_1.jpg");
+        MockMultipartFile file1 = new MockMultipartFile("images", "test_image_1.jpg", "image/jpg",
+                file1InputStream);
+
+        // 파일 2
+        InputStream file2InputStream = getClass().getResourceAsStream("/static/img/file/test_image_2.jpg");
+        MockMultipartFile file2 = new MockMultipartFile("images", "test_image_2.jpg", "image/jpg",
+                file2InputStream);
+
+        mockMvc.perform(multipart("/posts")
+                        .file(file1)
+                        .file(file2)
+                        .param("title", POST_REQUEST2_TITLE)
+                        .param("content", POST_REQUEST2_CONTENT)
+                        .param("price", String.valueOf(POST_REQUEST2_PRICE))
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                        .header(HttpHeaders.AUTHORIZATION, "Basic "
+                                + java.util.Base64.getEncoder().encodeToString("email:password".getBytes()))
+                )
+                .andExpect(status().isCreated());
+    }
+
+    private void 게시글_하나를_등록한다3() throws Exception {
+        // 파일 1
+        InputStream file1InputStream = getClass().getResourceAsStream("/static/img/file/test_image_1.jpg");
+        MockMultipartFile file1 = new MockMultipartFile("images", "test_image_1.jpg", "image/jpg",
+                file1InputStream);
+
+        // 파일 2
+        InputStream file2InputStream = getClass().getResourceAsStream("/static/img/file/test_image_2.jpg");
+        MockMultipartFile file2 = new MockMultipartFile("images", "test_image_2.jpg", "image/jpg",
+                file2InputStream);
+
+        mockMvc.perform(multipart("/posts")
+                        .file(file1)
+                        .file(file2)
+                        .param("title", POST_REQUEST3_TITLE)
+                        .param("content", POST_REQUEST3_CONTENT)
+                        .param("price", String.valueOf(POST_REQUEST3_PRICE))
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                        .header(HttpHeaders.AUTHORIZATION, "Basic "
+                                + java.util.Base64.getEncoder().encodeToString("email:password".getBytes()))
+                )
+                .andExpect(status().isCreated());
     }
 }
