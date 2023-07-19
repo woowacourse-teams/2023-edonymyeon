199653a605a1fb074e1a4cc33bf9ba9f8e49808a
김지현 <pig9619@naver.com>
2023-07-19 10:47:33 +0900

    feat/#13 게시글 추천 기능구현  (#60)
    * feat: 게시글 추천 기능/테스트 구현
    
    * refactor: 테스트 클래스 어노테이션 추가
    
    * refactor: 테스트 클래스 어노테이션 추가
    
    * refactor: transactional 어노테이션 추가
    
    * refactor: 서비스 로직 도메인으로 이동
    
    * refactor: 코드 컨벤션에 맞춰 수정함
    
    * refactor: ManyToOne으로 수정

---
+++ backend/src/main/java/edonymyeon/backend/thumbs/domain/Thumbs.java
@@ -0,0 +1,65 @@
+package edonymyeon.backend.thumbs.domain;
+
+import static edonymyeon.backend.global.exception.ExceptionInformation.THUMBS_UP_ALREADY_EXIST;
+
+import edonymyeon.backend.global.exception.EdonymyeonException;
+import edonymyeon.backend.member.domain.Member;
+import edonymyeon.backend.post.domain.Post;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Thumbs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Member member;
+
+    @Enumerated(value = EnumType.STRING)
+    private ThumbsType thumbsType;
+
+    public Thumbs(
+            final Post post,
+            final Member member,
+            final ThumbsType thumbsType
+    ) {
+        this.post = post;
+        this.member = member;
+        this.thumbsType = thumbsType;
+    }
+
+    private boolean isUp() {
+        return thumbsType == ThumbsType.UP;
+    }
+
+    private void updateThumbsType() {
+        this.thumbsType = thumbsType.getReverseType();
+    }
+
+    public void up() {
+        if (isUp()) {
+            throw new EdonymyeonException(THUMBS_UP_ALREADY_EXIST);
+        }
+        updateThumbsType();
+    }
+}
