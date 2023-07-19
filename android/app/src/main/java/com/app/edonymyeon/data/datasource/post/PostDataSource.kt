1de6f74c985eaa681fcc8b09b8d0a452c8f1f7ba
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-07-19 22:14:54 +0900

    feat: 게시글 상세 API 연동

--- android/app/src/main/java/com/app/edonymyeon/data/datasource/post/PostDataSource.kt
+++ android/app/src/main/java/com/app/edonymyeon/data/datasource/post/PostDataSource.kt
@@ -1,7 +1,8 @@
 package com.app.edonymyeon.data.datasource.post
 
 import com.app.edonymyeon.data.dto.response.PostDetailResponse
+import retrofit2.Response
 
 interface PostDataSource {
-    suspend fun getPostDetail(postId: Long): Call<PostDetailResponse>
+    suspend fun getPostDetail(postId: Long): Response<PostDetailResponse>
 }
--- android/app/src/main/java/com/app/edonymyeon/data/datasource/post/PostRemoteDataSource.kt
+++ android/app/src/main/java/com/app/edonymyeon/data/datasource/post/PostRemoteDataSource.kt
@@ -3,23 +3,13 @@ package com.app.edonymyeon.data.datasource.post
 import com.app.edonymyeon.data.dto.response.PostDetailResponse
 import com.app.edonymyeon.data.service.PostService
 import com.app.edonymyeon.data.service.client.RetrofitClient
-import okhttp3.ResponseBody
+import retrofit2.Response
 
 class PostRemoteDataSource : PostDataSource {
     private val postService: PostService =
         RetrofitClient.getInstance().create(PostService::class.java)
 
-    override suspend fun getPostDetail(postId: Long): Call<PostDetailResponse> {
-        val result = postService.getPost(postId)
-        return if (result.isSuccessful) {
-            Call.Success(result.body())
-        } else {
-            Call.Error(result.errorBody())
-        }
+    override suspend fun getPostDetail(postId: Long): Response<PostDetailResponse> {
+        return postService.getPost(postId)
     }
-}
-
-sealed class Call<T> {
-    data class Success<T>(val data: T?) : Call<T>()
-    data class Error<T>(val errorResult: ResponseBody?) : Call<T>()
 }
