4e04e21eb3aa5b040a502a9b8c89ce90346bd54e
Sujin Kim <sujinkim919@gmail.com>
2023-07-20 06:22:23 +0900

    feat: 스크랩 여부에 따라 하단 스크랩수 증가/감소 구현

--- android/app/src/main/java/com/app/edonymyeon/mapper/ReactionMapper.kt
+++ android/app/src/main/java/com/app/edonymyeon/mapper/ReactionMapper.kt
@@ -1,12 +1,21 @@
 package com.app.edonymyeon.mapper
 
 import com.app.edonymyeon.presentation.uimodel.ReactionCountUiModel
+import com.domain.edonymyeon.model.Count
 import com.domain.edonymyeon.model.ReactionCount
 
 fun ReactionCount.toUiModel(): ReactionCountUiModel {
     return ReactionCountUiModel(
-        viewCount = viewCount,
-        commentCount = commentCount,
-        scrapCount = scrapCount,
+        viewCount = viewCount.value,
+        commentCount = commentCount.value,
+        scrapCount = scrapCount.value,
+    )
+}
+
+fun ReactionCountUiModel.toDomain(): ReactionCount {
+    return ReactionCount(
+        viewCount = Count(viewCount),
+        commentCount = Count(commentCount),
+        scrapCount = Count(scrapCount),
     )
 }
