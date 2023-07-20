f13f6e907df51a744eedbf9d045644a590b0948d
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-07-20 14:00:02 +0900

    refactor: 작성된 게시글이나 수정된 게시글을 서버로 보내는 부분 multipar와 partmap 사용해서 수정

--- android/app/src/main/java/com/app/edonymyeon/data/datasource/post/PostRemoteDataSource.kt
+++ android/app/src/main/java/com/app/edonymyeon/data/datasource/post/PostRemoteDataSource.kt
@@ -1,15 +1,15 @@
 package com.app.edonymyeon.data.datasource.post
 
 import android.net.Uri
-import com.app.edonymyeon.data.dto.request.PartWrapper
-import com.app.edonymyeon.data.dto.request.PostEditorRequest
 import com.app.edonymyeon.data.dto.request.PostEditorResponse
 import com.app.edonymyeon.data.dto.response.PostDetailResponse
 import com.app.edonymyeon.data.service.PostService
 import com.app.edonymyeon.data.service.client.RetrofitClient
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Response
 import java.io.File
 
@@ -27,10 +27,13 @@ class PostRemoteDataSource : PostDataSource {
         price: Int,
         imageUris: List<Uri>,
     ): Response<PostEditorResponse> {
-        val partWrappers = generatePartWrappers(imageUris)
+        val images = generateMultiPart(imageUris)
+        val postEditorMap: HashMap<String, RequestBody> = hashMapOf()
+        postEditorMap["title"] = title.toRequestBody("text/plain".toMediaTypeOrNull())
+        postEditorMap["content"] = content.toRequestBody("text/plain".toMediaTypeOrNull())
+        postEditorMap["price"] = price.toString().toRequestBody("text/plain".toMediaTypeOrNull())
 
-        val postEditorRequest = PostEditorRequest(title, content, price, partWrappers)
-        return postService.savePost(postEditorRequest)
+        return postService.savePost(postEditorMap, images)
     }
 
     override suspend fun updatePost(
@@ -40,16 +43,19 @@ class PostRemoteDataSource : PostDataSource {
         price: Int,
         imageUris: List<Uri>,
     ): Response<PostEditorResponse> {
-        val partWrappers = generatePartWrappers(imageUris)
-        val postEditorRequest = PostEditorRequest(title, content, price, partWrappers)
-        return postService.updatePost(id, postEditorRequest)
+        val images = generateMultiPart(imageUris)
+        val postEditorMap: HashMap<String, RequestBody> = hashMapOf()
+        postEditorMap["title"] = title.toRequestBody("text/plain".toMediaTypeOrNull())
+        postEditorMap["content"] = content.toRequestBody("text/plain".toMediaTypeOrNull())
+        postEditorMap["price"] = price.toString().toRequestBody("text/plain".toMediaTypeOrNull())
+
+        return postService.updatePost(id, postEditorMap, images)
     }
 
-    private fun generatePartWrappers(imageUris: List<Uri>) =
+    private fun generateMultiPart(imageUris: List<Uri>) =
         imageUris.map { imageUri ->
             val file = File(imageUri.path ?: "")
             val requestFile = file.asRequestBody("image/*".toMediaTypeOrNull())
-            val part = MultipartBody.Part.createFormData("images[]", file.name, requestFile)
-            PartWrapper(part)
+            MultipartBody.Part.createFormData("images[]", file.name, requestFile)
         }
 }
