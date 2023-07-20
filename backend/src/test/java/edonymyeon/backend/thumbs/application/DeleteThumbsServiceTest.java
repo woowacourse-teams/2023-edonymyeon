b004130eb4f37d272cd27d1e3f9737fceaf311f8
김지현 <pig9619@naver.com>
2023-07-20 11:16:25 +0900

    Feat/#61 게시글 추천/비추천 취소기능 구현 (#82)
    * feat: 게시글 추천/비추천 취소 기능 구현
    
    * test: 도메인 테스트 추가
    
    * refactor: 코드 수정
    
    * refactor: memberTestSupport 사용하도록 수정
    
    * refactor: 리뷰 반영 수정
    
    * refactor: 로그인 하지 않은 멤버에 대한 로직 수정

--- backend/src/test/java/edonymyeon/backend/thumbs/application/DeleteThumbsServiceTest.java
+++ backend/src/test/java/edonymyeon/backend/thumbs/application/DeleteThumbsServiceTest.java
@@ -1,10 +1,10 @@
 package edonymyeon.backend.thumbs.application;
 
-import static edonymyeon.backend.global.exception.ExceptionInformation.POST_ID_NOT_FOUND;
-import static edonymyeon.backend.global.exception.ExceptionInformation.THUMBS_DOWN_ALREADY_EXIST;
-import static edonymyeon.backend.global.exception.ExceptionInformation.THUMBS_IS_SELF_UP_DOWN;
+import static edonymyeon.backend.global.exception.ExceptionInformation.THUMBS_DOWN_DELETE_FAIL_WHEN_THUMBS_UP;
+import static edonymyeon.backend.global.exception.ExceptionInformation.THUMBS_DOWN_IS_NOT_EXIST;
+import static edonymyeon.backend.global.exception.ExceptionInformation.THUMBS_UP_DELETE_FAIL_WHEN_THUMBS_DOWN;
+import static edonymyeon.backend.global.exception.ExceptionInformation.THUMBS_UP_IS_NOT_EXIST;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import edonymyeon.backend.global.exception.EdonymyeonException;
 import edonymyeon.backend.member.application.dto.MemberIdDto;
@@ -15,10 +15,10 @@ import edonymyeon.backend.post.application.dto.PostRequest;
 import edonymyeon.backend.post.application.dto.PostResponse;
 import edonymyeon.backend.support.MemberTestSupport;
 import edonymyeon.backend.thumbs.domain.Thumbs;
-import edonymyeon.backend.thumbs.domain.ThumbsType;
 import edonymyeon.backend.thumbs.repository.ThumbsRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -35,7 +35,7 @@ import org.springframework.transaction.annotation.Transactional;
 @TestConstructor(autowireMode = AutowireMode.ALL)
 @Transactional
 @SpringBootTest
-public class ThumbsDownServiceTest {
+public class DeleteThumbsServiceTest {
 
     private final ThumbsService thumbsService;
 
@@ -45,13 +45,11 @@ public class ThumbsDownServiceTest {
 
     private final MemberTestSupport memberTestSupport;
 
-    private Member postWriter;
-
     private PostResponse postResponse;
 
     @BeforeEach
     public void 회원가입과_게시글쓰기를_한다() {
-        postWriter =  registerMember();
+        Member postWriter = registerMember();
         PostRequest postRequest = new PostRequest(
                 "title",
                 "content",
@@ -64,80 +62,71 @@ public class ThumbsDownServiceTest {
     }
 
     @Test
-    void 비추천하려는_게시물이_없으면_예외가_발생한다() {
-        MemberIdDto loginMemberId = new MemberIdDto(postWriter.getId());
-
-        assertThatThrownBy(
-                () -> thumbsService.thumbsDown(loginMemberId, 1000L))
-                .isExactlyInstanceOf(EdonymyeonException.class)
-                .hasMessage(POST_ID_NOT_FOUND.getMessage());
-    }
-
-    @Test
-    void 비추천한_게시물이_로그인한_사람이_작성한_것이라면_예외가_발생한다() {
-        MemberIdDto loginMemberId = new MemberIdDto(postWriter.getId());
-
-        assertThatThrownBy(
-                () -> thumbsService.thumbsDown(loginMemberId, postResponse.id()))
-                .isExactlyInstanceOf(EdonymyeonException.class)
-                .hasMessage(THUMBS_IS_SELF_UP_DOWN.getMessage());
-    }
-
-    @Test
-    void 비추천시_해당게시물에_비추천한_적_없으면_추가한다(@Autowired ThumbsRepository thumbsRepository) {
+    void 추천_취소(@Autowired ThumbsRepository thumbsRepository) {
         // given
         Member otherMember = registerMember();
-
-        // when
-        thumbsDown(otherMember, postResponse);
-        Optional<Thumbs> postThumbs = thumbsRepository.findByPostIdAndMemberId(postResponse.id(),
-                otherMember.getId());
-
-        // then
-        assertSoftly(softly -> {
-                    softly.assertThat(postThumbs).isPresent();
-                    softly.assertThat(postThumbs.get().getThumbsType()).isEqualTo(ThumbsType.DOWN);
-                    softly.assertThat(postThumbs.get().getMember().getId()).isEqualTo(otherMember.getId());
-                    softly.assertThat(postThumbs.get().getPost().getId()).isEqualTo(postResponse.id());
-                }
-        );
-    }
-
-    @Test
-    void 비추천시_해당게시물에_추천한_적_있으면_비추천으로_업데이트_된다(@Autowired ThumbsRepository thumbsRepository) {
-        // given
-        Member otherMember = registerMember();
-
-        // when
         thumbsUp(otherMember, postResponse);
-        thumbsDown(otherMember, postResponse);
 
+        // when
+        deleteThumbsUp(otherMember, postResponse);
         Optional<Thumbs> postThumbs = thumbsRepository.findByPostIdAndMemberId(postResponse.id(),
                 otherMember.getId());
 
         // then
-        assertSoftly(softly -> {
-                    softly.assertThat(postThumbs).isPresent();
-                    softly.assertThat(postThumbs.get().getThumbsType()).isEqualTo(ThumbsType.DOWN);
-                    softly.assertThat(postThumbs.get().getMember().getId()).isEqualTo(otherMember.getId());
-                    softly.assertThat(postThumbs.get().getPost().getId()).isEqualTo(postResponse.id());
-                }
-        );
+        Assertions.assertThat(postThumbs).isEmpty();
     }
 
     @Test
-    void 비추천시_해당게시물에_이미_비추천되어_있으면_예외가_발생한다() {
-        // given
+    void 추천이_되어있지_않은경우_추천취소를_할_수_없다() {
         Member otherMember = registerMember();
 
-        // when
-        MemberIdDto otherMemberId = new MemberIdDto(otherMember.getId());
+        assertThatThrownBy(() -> deleteThumbsUp(otherMember, postResponse))
+                .isExactlyInstanceOf(EdonymyeonException.class)
+                .hasMessage(THUMBS_UP_IS_NOT_EXIST.getMessage());
+    }
+
+    @Test
+    void 비추천인_상태일때_추천을_취소할_수_없다() {
+        Member otherMember = registerMember();
         thumbsDown(otherMember, postResponse);
 
-        assertThatThrownBy(
-                () -> thumbsService.thumbsDown(otherMemberId, postResponse.id()))
+        assertThatThrownBy(() -> deleteThumbsUp(otherMember, postResponse))
                 .isExactlyInstanceOf(EdonymyeonException.class)
-                .hasMessage(THUMBS_DOWN_ALREADY_EXIST.getMessage());
+                .hasMessage(THUMBS_UP_DELETE_FAIL_WHEN_THUMBS_DOWN.getMessage());
+    }
+
+    @Test
+    void 비추천_취소(@Autowired ThumbsRepository thumbsRepository) {
+        // given
+        Member otherMember = registerMember();
+        thumbsDown(otherMember, postResponse);
+
+        // when
+        deleteThumbsDown(otherMember, postResponse);
+        Optional<Thumbs> postThumbs = thumbsRepository.findByPostIdAndMemberId(postResponse.id(),
+                otherMember.getId());
+
+        // then
+        Assertions.assertThat(postThumbs).isEmpty();
+    }
+
+    @Test
+    void 비추천이_되어있지_않은경우_비추천취소를_할_수_없다() {
+        Member otherMember = registerMember();
+
+        assertThatThrownBy(() -> deleteThumbsDown(otherMember, postResponse))
+                .isExactlyInstanceOf(EdonymyeonException.class)
+                .hasMessage(THUMBS_DOWN_IS_NOT_EXIST.getMessage());
+    }
+
+    @Test
+    void 추천인_상태일때_비추천을_취소할_수_없다() {
+        Member otherMember = registerMember();
+        thumbsUp(otherMember, postResponse);
+
+        assertThatThrownBy(() -> deleteThumbsDown(otherMember, postResponse))
+                .isExactlyInstanceOf(EdonymyeonException.class)
+                .hasMessage(THUMBS_DOWN_DELETE_FAIL_WHEN_THUMBS_UP.getMessage());
     }
 
     private void thumbsUp(final Member member, final PostResponse post) {
@@ -148,6 +137,16 @@ public class ThumbsDownServiceTest {
     private void thumbsDown(final Member member, final PostResponse post) {
         MemberIdDto memberId = new MemberIdDto(member.getId());
         thumbsService.thumbsDown(memberId, post.id());
+    }
+
+    private void deleteThumbsUp(final Member member, final PostResponse post) {
+        MemberIdDto memberId = new MemberIdDto(member.getId());
+        thumbsService.deleteThumbsUp(memberId, post.id());
+    }
+
+    private void deleteThumbsDown(final Member member, final PostResponse post) {
+        MemberIdDto memberId = new MemberIdDto(member.getId());
+        thumbsService.deleteThumbsDown(memberId, post.id());
     }
 
     private Member registerMember() {
