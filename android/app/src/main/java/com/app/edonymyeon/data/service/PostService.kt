5a8de9c9c4325955539ec0fb99660f01a50eaf8e
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-07-20 14:00:02 +0900

    refactor: 작성된 게시글이나 수정된 게시글을 서버로 보내는 부분 multipar와 partmap 사용해서 수정

--- android/app/src/main/java/com/app/edonymyeon/data/service/PostService.kt
+++ android/app/src/main/java/com/app/edonymyeon/data/service/PostService.kt
@@ -1,27 +1,34 @@
 package com.app.edonymyeon.data.service
 
-import com.app.edonymyeon.data.dto.request.PostEditorRequest
 import com.app.edonymyeon.data.dto.request.PostEditorResponse
 import com.app.edonymyeon.data.dto.response.PostDetailResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Part
+import retrofit2.http.PartMap
 import retrofit2.http.Path
 
 interface PostService {
     @GET("/posts/{postId}")
     suspend fun getPost(@Path("postId") postId: Long): Response<PostDetailResponse>
 
+    @Multipart
     @POST("/posts")
     suspend fun savePost(
-        @Body postEditorRequest: PostEditorRequest,
+        @PartMap postEditorRequest: HashMap<String, RequestBody>,
+        @Part images: List<MultipartBody.Part>,
     ): Response<PostEditorResponse>
 
+    @Multipart
     @PUT("/posts/{postId}")
     suspend fun updatePost(
         @Path("postId") postId: Long,
-        @Body postEditorRequest: PostEditorRequest,
+        @PartMap postEditorRequest: HashMap<String, RequestBody>,
+        @Part images: List<MultipartBody.Part>,
     ): Response<PostEditorResponse>
 }
