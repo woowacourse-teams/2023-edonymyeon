ec8db5ade5825cab861112dbbc05e7ada89a0296
hectick <ch.yeondev@gmail.com>
2023-07-28 16:58:11 +0900

    test: log().all() 제거

--- backend/src/test/java/edonymyeon/backend/post/integration/PostIntegrationTest.java
+++ backend/src/test/java/edonymyeon/backend/post/integration/PostIntegrationTest.java
@@ -236,10 +236,10 @@ public class PostIntegrationTest extends IntegrationTest implements ImageFileCle
         final PostResponse postResponse = postService.createPost(new MemberIdDto(member.getId()), postRequest);
 
         final ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
+                .given()
                 .when()
                 .get("/posts/" + postResponse.id())
-                .then().log().all()
+                .then()
                 .extract();
 
         final SpecificPostInfoResponse post = postService.findSpecificPost(postResponse.id(),
@@ -286,11 +286,11 @@ public class PostIntegrationTest extends IntegrationTest implements ImageFileCle
         final PostResponse postResponse = postService.createPost(new MemberIdDto(member.getId()), postRequest);
 
         final ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
+                .given()
                 .when()
                 .auth().preemptive().basic(member.getEmail(), member.getPassword())
                 .get("/posts/" + postResponse.id())
-                .then().log().all()
+                .then()
                 .extract();
 
         final SpecificPostInfoResponse post = postService.findSpecificPost(postResponse.id(),
@@ -341,11 +341,11 @@ public class PostIntegrationTest extends IntegrationTest implements ImageFileCle
         final PostResponse postResponse = postService.createPost(new MemberIdDto(writer.getId()), postRequest);
 
         final ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
+                .given()
                 .when()
                 .auth().preemptive().basic(reader.getEmail(), reader.getPassword())
                 .get("/posts/" + postResponse.id())
-                .then().log().all()
+                .then()
                 .extract();
 
         final SpecificPostInfoResponse post = postService.findSpecificPost(postResponse.id(),
