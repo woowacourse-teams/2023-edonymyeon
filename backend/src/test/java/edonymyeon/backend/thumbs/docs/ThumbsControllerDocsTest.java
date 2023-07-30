d66f358b2337b3a157f8c014ff613ef11283eb9b
hectick <ch.yeondev@gmail.com>
2023-07-31 01:02:44 +0900

    test: 좋아요 문서화 코드에 path 파라미터도 문서화하는 코드 추가

--- backend/src/test/java/edonymyeon/backend/thumbs/docs/ThumbsControllerDocsTest.java
+++ backend/src/test/java/edonymyeon/backend/thumbs/docs/ThumbsControllerDocsTest.java
@@ -4,6 +4,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import edonymyeon.backend.member.domain.Member;
@@ -52,13 +54,14 @@ public class ThumbsControllerDocsTest {
     private Post 글;
 
     @BeforeEach
-    void 회원_두명_가입하고_글쓰기_모킹(){
+    void 회원_두명_가입하고_글쓰기_모킹() {
         글쓴이 = new Member(1L, "email", "password", "nickname", null);
         when(memberRepository.findByEmailAndPassword(글쓴이.getEmail(), 글쓴이.getPassword())).thenReturn(Optional.of(글쓴이));
         when(memberRepository.findById(글쓴이.getId())).thenReturn(Optional.of(글쓴이));
 
         반응_하는_사람 = new Member(2L, "email2", "password2", "nickname2", null);
-        when(memberRepository.findByEmailAndPassword(반응_하는_사람.getEmail(), 반응_하는_사람.getPassword())).thenReturn(Optional.of(반응_하는_사람));
+        when(memberRepository.findByEmailAndPassword(반응_하는_사람.getEmail(), 반응_하는_사람.getPassword())).thenReturn(
+                Optional.of(반응_하는_사람));
         when(memberRepository.findById(반응_하는_사람.getId())).thenReturn(Optional.of(반응_하는_사람));
 
         글 = new Post(1L, "title", "content", 1_000L, 글쓴이, null, LocalDateTime.now(), 0L);
@@ -70,14 +73,18 @@ public class ThumbsControllerDocsTest {
         Thumbs 조와요 = new Thumbs(글, 글쓴이, ThumbsType.UP);
         when(thumbsRepository.save(조와요)).thenReturn(조와요);
 
-        final MockHttpServletRequestBuilder 좋아요_요청 = put("/posts/" + 글.getId() + "/up")
+        final MockHttpServletRequestBuilder 좋아요_요청 = put("/posts/{postId}/up", 글.getId())
                 .header(HttpHeaders.AUTHORIZATION, "Basic "
                         + java.util.Base64.getEncoder()
                         .encodeToString((반응_하는_사람.getEmail() + ":" + 반응_하는_사람.getPassword()).getBytes()));
 
         this.mockMvc.perform(좋아요_요청)
                 .andExpect(status().isOk())
-                .andDo(document("thumbs-up"));
+                .andDo(document("thumbs-up",
+                        pathParameters(
+                                parameterWithName("postId").description("게시글 id")
+                        )
+                ));
     }
 
     @Test
@@ -85,14 +92,18 @@ public class ThumbsControllerDocsTest {
         Thumbs 시러요 = new Thumbs(글, 글쓴이, ThumbsType.DOWN);
         when(thumbsRepository.save(시러요)).thenReturn(시러요);
 
-        final MockHttpServletRequestBuilder 싫어요_요청 = put("/posts/" + 글.getId() + "/down")
+        final MockHttpServletRequestBuilder 싫어요_요청 = put("/posts/{postId}/down", 글.getId())
                 .header(HttpHeaders.AUTHORIZATION, "Basic "
                         + java.util.Base64.getEncoder()
                         .encodeToString((반응_하는_사람.getEmail() + ":" + 반응_하는_사람.getPassword()).getBytes()));
 
         this.mockMvc.perform(싫어요_요청)
                 .andExpect(status().isOk())
-                .andDo(document("thumbs-down"));
+                .andDo(document("thumbs-down",
+                        pathParameters(
+                                parameterWithName("postId").description("게시글 id")
+                        )
+                ));
     }
 
     @Test
@@ -100,14 +111,18 @@ public class ThumbsControllerDocsTest {
         Thumbs 좋아요 = new Thumbs(글, 글쓴이, ThumbsType.UP);
         when(thumbsRepository.findByPostIdAndMemberId(글.getId(), 반응_하는_사람.getId())).thenReturn(Optional.of(좋아요));
 
-        final MockHttpServletRequestBuilder 좋아요_취소_요청 = delete("/posts/" + 글.getId() + "/up")
+        final MockHttpServletRequestBuilder 좋아요_취소_요청 = delete("/posts/{postId}/up", 글.getId())
                 .header(HttpHeaders.AUTHORIZATION, "Basic "
                         + java.util.Base64.getEncoder()
                         .encodeToString((반응_하는_사람.getEmail() + ":" + 반응_하는_사람.getPassword()).getBytes()));
 
         this.mockMvc.perform(좋아요_취소_요청)
                 .andExpect(status().isOk())
-                .andDo(document("thumbs-up-delete"));
+                .andDo(document("thumbs-up-delete",
+                        pathParameters(
+                                parameterWithName("postId").description("게시글 id")
+                        )
+                ));
     }
 
     @Test
@@ -115,13 +130,17 @@ public class ThumbsControllerDocsTest {
         Thumbs 싫어요 = new Thumbs(글, 글쓴이, ThumbsType.DOWN);
         when(thumbsRepository.findByPostIdAndMemberId(글.getId(), 반응_하는_사람.getId())).thenReturn(Optional.of(싫어요));
 
-        final MockHttpServletRequestBuilder 싫어요_취소_요청 = delete("/posts/" + 글.getId() + "/down")
+        final MockHttpServletRequestBuilder 싫어요_취소_요청 = delete("/posts/{postId}/down", 글.getId())
                 .header(HttpHeaders.AUTHORIZATION, "Basic "
                         + java.util.Base64.getEncoder()
                         .encodeToString((반응_하는_사람.getEmail() + ":" + 반응_하는_사람.getPassword()).getBytes()));
 
         this.mockMvc.perform(싫어요_취소_요청)
                 .andExpect(status().isOk())
-                .andDo(document("thumbs-down-delete"));
+                .andDo(document("thumbs-down-delete",
+                        pathParameters(
+                                parameterWithName("postId").description("게시글 id")
+                        )
+                ));
     }
 }
