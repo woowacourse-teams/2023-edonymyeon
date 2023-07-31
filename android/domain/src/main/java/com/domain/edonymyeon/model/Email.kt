4bef1775d4ffe75963748b251842f9ba6b8cc00a
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-07-31 13:35:53 +0900

    feat: 회원가입 도메인 모델 추가

---
+++ android/domain/src/main/java/com/domain/edonymyeon/model/Email.kt
@@ -0,0 +1,20 @@
+package com.domain.edonymyeon.model
+
+@JvmInline
+value class Email private constructor(val value: String) {
+    init {
+        require(validate(value)) { ERROR_NEGATIVE_EMAIL }
+    }
+
+    companion object {
+        fun validate(value: String): Boolean = Regex(Email_REGEX).matches(value)
+
+        fun create(value: String): Email {
+            return Email(value.trim())
+        }
+
+        private const val Email_REGEX =
+            "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\$"
+        private const val ERROR_NEGATIVE_EMAIL = "이메일 형식이 맞지 않습니다."
+    }
+}
