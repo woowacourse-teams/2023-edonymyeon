87643bca4c7426d547f63405442cc86d9fbdcf49
BackFoxx <backfoxxx@gmail.com>
2023-07-31 14:01:54 +0900

    docs: LoggingFilter와 LoggingInterceptor의 역할을 javadoc으로 설명

--- backend/src/main/java/edonymyeon/backend/global/logging/LoggingFilter.java
+++ backend/src/main/java/edonymyeon/backend/global/logging/LoggingFilter.java
@@ -10,8 +10,16 @@ import java.io.IOException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
+/**
+ * 서블릿 필터 사이에 개입하여,
+ * ServletResponse를 ContentCachingResponseWrapper로 바꾸어 체이닝에 등록하는 필터입니다.
+ * ContentCachingResponseWrapper를 사용하면
+ * ServletResponse와 달리 responseBody 내용을 꺼내 쓸 수 있기 때문에
+ * 로깅에 활용할 수 있습니다.
+**/
 @Component
 public class LoggingFilter implements Filter {
+
     @Override
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
             throws IOException, ServletException {
--- backend/src/main/java/edonymyeon/backend/global/logging/LoggingInterceptor.java
+++ backend/src/main/java/edonymyeon/backend/global/logging/LoggingInterceptor.java
@@ -2,6 +2,7 @@ package edonymyeon.backend.global.logging;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Objects;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.web.util.ContentCachingResponseWrapper;
 
 @Slf4j
 public class LoggingInterceptor implements HandlerInterceptor {
+
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler)
             throws Exception {
@@ -38,20 +40,24 @@ public class LoggingInterceptor implements HandlerInterceptor {
                                 final Object handler, final Exception ex)
             throws Exception {
         MDC.put("statuscode", String.valueOf(response.getStatus()));
-
-        ContentCachingResponseWrapper responseWrapper = getResponseWrapper(response);
-        String responseBody = getResponseBody(responseWrapper);
-        log.info("body {} ", responseBody);
-        // 클라이언트로 전달 전 실제 response 객체에 copy
-        responseWrapper.copyBodyToResponse();
-
+        logResponseBody(response);
         MDC.clear();
+    }
+
+    private void logResponseBody(final HttpServletResponse response) throws IOException {
+        ContentCachingResponseWrapper responseWrapper = getResponseWrapper(response);
+        log.info("body {} ", getResponseBody(responseWrapper));
+        responseWrapper.copyBodyToResponse();
     }
 
     private String getResponseBody(ContentCachingResponseWrapper responseWrapper) {
         return new String(responseWrapper.getContentAsByteArray());
     }
 
+    /***
+     * HttpServletResponse를 ContentCachingResponseWrapper로 형변환하는 메소드입니다.
+     * ContentCachingResponseWrapper로부터 ResponseBody 내용을 추출할 수 있습니다.
+     */
     private ContentCachingResponseWrapper getResponseWrapper(HttpServletResponse response) {
         if (response instanceof ContentCachingResponseWrapper) {
             return (ContentCachingResponseWrapper)response;
