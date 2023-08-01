eb52b8412657aea6992ba7d1f57f575db566d299
hectick <ch.yeondev@gmail.com>
2023-08-01 12:42:35 +0900

    refactor: 안쓰는 메서드 제거

--- backend/src/main/java/edonymyeon/backend/consumption/domain/ConsumptionType.java
+++ backend/src/main/java/edonymyeon/backend/consumption/domain/ConsumptionType.java
@@ -1,16 +1,7 @@
 package edonymyeon.backend.consumption.domain;
 
-import java.util.Objects;
-
 public enum ConsumptionType {
 
     PURCHASE,
     SAVING;
-
-    public static ConsumptionType classifyConsumptionType(final Long purchasePrice) {
-        if (Objects.isNull(purchasePrice)) {
-            return SAVING;
-        }
-        return PURCHASE;
-    }
 }
