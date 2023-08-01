623fe00ab2d2f40cefcb205e7036524f75a841f2
This2sho <zx00018@naver.com>
2023-08-01 22:14:34 +0900

    feat: 내가 쓴 글 조회 기능 구현

--- backend/src/main/java/edonymyeon/backend/member/application/MemberService.java
+++ backend/src/main/java/edonymyeon/backend/member/application/MemberService.java
@@ -2,15 +2,27 @@ package edonymyeon.backend.member.application;
 
 import static edonymyeon.backend.global.exception.ExceptionInformation.MEMBER_ID_NOT_FOUND;
 
+import edonymyeon.backend.consumption.domain.Consumption;
+import edonymyeon.backend.consumption.repository.ConsumptionRepository;
 import edonymyeon.backend.global.exception.EdonymyeonException;
+import edonymyeon.backend.image.domain.Domain;
 import edonymyeon.backend.member.application.dto.MemberIdDto;
-import edonymyeon.backend.member.application.dto.MyPageResponse;
 import edonymyeon.backend.member.application.dto.YearMonthDto;
 import edonymyeon.backend.member.application.dto.request.PurchaseConfirmRequest;
 import edonymyeon.backend.member.application.dto.request.SavingConfirmRequest;
+import edonymyeon.backend.member.application.dto.response.MyPageResponse;
+import edonymyeon.backend.member.application.dto.response.MyPostResponse;
 import edonymyeon.backend.member.domain.Member;
 import edonymyeon.backend.member.repository.MemberRepository;
+import edonymyeon.backend.post.domain.Post;
+import edonymyeon.backend.post.repository.PostRepository;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,12 +33,37 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    private final PostRepository postRepository;
+
     private final ConsumptionConfirmService consumptionConfirmService;
+
+    private final ConsumptionRepository consumptionRepository;
+
+    private final Domain domain;
 
     public MyPageResponse findMemberInfoById(final Long id) {
         final Member member = memberRepository.findById(id)
                 .orElseThrow(() -> new EdonymyeonException(MEMBER_ID_NOT_FOUND));
         return new MyPageResponse(member.getId(), member.getNickname());
+    }
+
+    public Slice<MyPostResponse> findMyPosts(final MemberIdDto memberIdDto, Pageable pageable) {
+        final Slice<Post> posts = postRepository.findAllByMemberId(memberIdDto.id(), pageable);
+        final List<Long> postIds = posts.map(Post::getId)
+                .toList();
+
+        final List<Consumption> consumptions = consumptionRepository.findAllByPostIds(postIds);
+
+        final Map<Long, Consumption> consumptionsByPostId = consumptions.stream()
+                .collect(Collectors.toMap(consumption -> consumption.getPost().getId(), Function.identity()));
+
+        return posts.map(post -> {
+            if (consumptionsByPostId.containsKey(post.getId())) {
+                final Consumption consumption = consumptionsByPostId.get(post.getId());
+                return MyPostResponse.of(post, domain, consumption);
+            }
+            return MyPostResponse.of(post, domain);
+        });
     }
 
     public void confirmPurchase(
