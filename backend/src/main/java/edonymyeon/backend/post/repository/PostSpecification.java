b71155a12d8b6ba76fee68c1e547adc86879eb4e
jyeost <pig9619@naver.com>
2023-08-02 10:54:52 +0900

    feat: 띄어쓰기를 기준으로 다중 키워드로 검색 가능하도록 구현

--- backend/src/main/java/edonymyeon/backend/post/repository/PostSpecification.java
+++ backend/src/main/java/edonymyeon/backend/post/repository/PostSpecification.java
@@ -1,14 +1,40 @@
 package edonymyeon.backend.post.repository;
 
 import edonymyeon.backend.post.domain.Post;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class PostSpecification {
 
     public static Specification<Post> searchBy(String searchWord) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.or(
-                criteriaBuilder.like(root.get("title"), "%" + searchWord + "%"),
-                criteriaBuilder.like(root.get("content"), "%" + searchWord + "%")
-        );
+        return ((root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (searchWord != null && !searchWord.isBlank()) {
+                appendSearchCondition(searchWord, root, criteriaBuilder, predicates);
+            }
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        });
+    }
+
+    private static void appendSearchCondition(String searchWord, Root<Post> root, CriteriaBuilder criteriaBuilder, List<Predicate> predicates) {
+        List<String> searchWords = splitKeyWordByBlank(searchWord);
+        for (String word : searchWords) {
+            predicates.add(criteriaBuilder.or(
+                    criteriaBuilder.like(root.get("title"), "%" + word + "%"),
+                    criteriaBuilder.like(root.get("content"), "%" + word + "%")
+            ));
+        }
+    }
+
+    private static List<String> splitKeyWordByBlank(String searchWord) {
+        return Arrays.stream(searchWord.strip().split(" "))
+                .filter(word -> !word.isBlank())
+                .toList();
     }
 }
--- backend/src/test/java/edonymyeon/backend/post/application/PostServiceSearchPostsTest.java
+++ backend/src/test/java/edonymyeon/backend/post/application/PostServiceSearchPostsTest.java
@@ -1,8 +1,5 @@
 package edonymyeon.backend.post.application;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
-
 import edonymyeon.backend.member.domain.Member;
 import edonymyeon.backend.post.application.dto.GeneralFindingCondition;
 import edonymyeon.backend.support.MemberTestSupport;
@@ -16,6 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestConstructor.AutowireMode;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 @SuppressWarnings("NonAsciiCharacters")
 @RequiredArgsConstructor
@@ -43,14 +43,14 @@ public class PostServiceSearchPostsTest {
         postId1 = postTestSupport.builder()
                 .member(member)
                 .title("애플 먹고 싶어요")
-                .content("사먹어도 되나요? 자취생인데...")
+                .content("사먹어도 되나요? 대학생 자취생인데... 거지인데...")
                 .build()
                 .getId();
 
         postId2 = postTestSupport.builder()
                 .member(member)
                 .title("사과 먹고 싶어요")
-                .content("사먹어도 되나요? 자취생인데...")
+                .content("사먹어도 되나요? 사회 초년생 자취생인데... 거지인데...")
                 .build()
                 .getId();
     }
@@ -98,5 +98,44 @@ public class PostServiceSearchPostsTest {
 
         // then
         assertThat(검색결과.size()).isEqualTo(0);
+    }
+
+    @Test
+    void 키워드로_검색했을때_내용이_제대로_검색되는지_확인한다() {
+        // when
+        var 검색결과 = postService.searchPosts(" 사회   초년 자취   거지 ", emptyFindingCondition).posts();
+
+        // then
+        assertSoftly(softly -> {
+                    softly.assertThat(검색결과.size()).isEqualTo(1);
+                    softly.assertThat(검색결과.get(0).id()).isEqualTo(postId2);
+                }
+        );
+    }
+
+    @Test
+    void 키워드로_앞뒤_순서가_바뀌었을때_내용이_제대로_검색되는지_확인한다() {
+        // when
+        var 검색결과 = postService.searchPosts(" 거지 자취 사회  초년", emptyFindingCondition).posts();
+
+        // then
+        assertSoftly(softly -> {
+                    softly.assertThat(검색결과.size()).isEqualTo(1);
+                    softly.assertThat(검색결과.get(0).id()).isEqualTo(postId2);
+                }
+        );
+    }
+
+    @Test
+    void 키워드로_동일한_키워드가_두개_포함되어도_내용이_제대로_검색되는지_확인한다() {
+        // when
+        var 검색결과 = postService.searchPosts(" 거지 자취 사회 거지 초년", emptyFindingCondition).posts();
+
+        // then
+        assertSoftly(softly -> {
+                    softly.assertThat(검색결과.size()).isEqualTo(1);
+                    softly.assertThat(검색결과.get(0).id()).isEqualTo(postId2);
+                }
+        );
     }
 }
