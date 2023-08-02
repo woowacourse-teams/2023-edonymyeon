b330d21bd6a63ce68409e30d71548fc006c7f959
로피 <50761690+Choisehyeon@users.noreply.github.com>
2023-08-02 14:51:53 +0900

    Feat/#128 내가쓴글 페이지 뷰 구현 (#160)
    * feat: 내가 쓴 글 조회 페이지 뷰를 만든다
    - 소비확정 버튼을 누르면 다이얼로그가 열린다.
    - 다이얼로그는 소비금액, 확정일자를 입력받을 수 있다.
    
    * chore: log 제거
    
    * refactor: binding 초기화 방법 수정
    
    * refactor: 앱바 타이틀 없애도록 수정
    
    * feat: 팩토리 메서드 구현
    
    * refactor: xml 수정
    
    * refactor: 생명주기에 맞게 뷰 구현하는 부분 이동

---
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/mypost/adapter/MyPostAdapter.kt
@@ -0,0 +1,19 @@
+package com.app.edonymyeon.presentation.ui.mypost.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.app.edonymyeon.presentation.ui.mypost.listener.MyPostClickListener
+import com.app.edonymyeon.presentation.ui.mypost.viewholder.MyPostViewHolder
+import com.app.edonymyeon.presentation.uimodel.MyPostUiModel
+
+class MyPostAdapter(
+    private val clickListener: MyPostClickListener,
+) : ListAdapter<MyPostUiModel, MyPostViewHolder>(MyPostDiffUtilCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPostViewHolder {
+        return MyPostViewHolder(parent, clickListener)
+    }
+
+    override fun onBindViewHolder(holder: MyPostViewHolder, position: Int) {
+        return holder.bind(currentList[position])
+    }
+}
