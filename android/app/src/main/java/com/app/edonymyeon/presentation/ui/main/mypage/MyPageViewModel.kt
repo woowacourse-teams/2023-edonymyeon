578d3f52abc235495dd32fecac902dc76d70053d
Sujin Kim <sujinkim919@gmail.com>
2023-08-03 10:38:28 +0900

    Feat/#135 그래프 (#171)
    * feat: 라인 2개가 그려지는 그래프 구현
    
    - MPAndroidChart 적용
    
    * feat: 더미 데이터를 view model의 live data 이용
    
    - 두 연/월 사이에 있는 날짜들을 구하는 로직은 도메인에서 진행
    
    * feat: 그래프 클릭 시 해당 포인트의 가격 값 보여주도록 구현
    
    - MarkerView 이용
    
    * refactor: 그래프를 별도의 클래스로 분리 및 주석 추가
    
    * refactor: 클래스 이름 변경

---
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/main/mypage/MyPageViewModel.kt
@@ -0,0 +1,28 @@
+package com.app.edonymyeon.presentation.ui.main.mypage
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.app.edonymyeon.mapper.toDomain
+import com.app.edonymyeon.presentation.uimodel.ConsumptionAmountUiModel
+import com.app.edonymyeon.presentation.uimodel.ConsumptionStatisticsUiModel
+import java.time.YearMonth
+import kotlin.random.Random
+
+class MyPageViewModel : ViewModel() {
+    private val _consumptions = MutableLiveData<ConsumptionStatisticsUiModel>()
+    val consumptions: LiveData<ConsumptionStatisticsUiModel>
+        get() = _consumptions
+
+    fun getMonthLists(): List<String> = _consumptions.value?.toDomain()?.monthRange?.yearMonthList ?: emptyList()
+
+    fun setConsumptions() {
+        _consumptions.value = ConsumptionStatisticsUiModel(
+            startMonth = YearMonth.parse("2023-02"),
+            endMonth = YearMonth.parse("2023-07"),
+            consumptionAmounts = List(6) {
+                ConsumptionAmountUiModel(Random.nextInt(10000, 100000), Random.nextInt(10000, 100000))
+            },
+        )
+    }
+}
