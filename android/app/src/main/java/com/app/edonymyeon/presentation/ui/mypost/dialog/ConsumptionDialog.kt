8b31318a8b774077fed6426a4bdfce42ca4232c0
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-08-03 13:26:56 +0900

    chore: lint 수정

--- android/app/src/main/java/com/app/edonymyeon/presentation/ui/mypost/dialog/ConsumptionDialog.kt
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/mypost/dialog/ConsumptionDialog.kt
@@ -3,9 +3,7 @@ package com.app.edonymyeon.presentation.ui.mypost.dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import app.edonymyeon.databinding.DialogInputConsumptionBinding
 
@@ -13,13 +11,6 @@ class ConsumptionDialog : DialogFragment() {
 
     private val binding by lazy {
         DialogInputConsumptionBinding.inflate(layoutInflater)
-    }
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
