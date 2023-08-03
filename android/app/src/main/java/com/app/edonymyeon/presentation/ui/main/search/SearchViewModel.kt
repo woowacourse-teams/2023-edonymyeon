dc89ce68f5de1dde7a28008e0ddae16c9e4f9c8e
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-08-03 12:43:20 +0900

    feat: 검색 기능 추가

--- android/app/src/main/java/com/app/edonymyeon/presentation/ui/main/search/SearchViewModel.kt
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/main/search/SearchViewModel.kt
@@ -1,5 +1,33 @@
 package com.app.edonymyeon.presentation.ui.main.search
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.app.edonymyeon.mapper.toUiModel
+import com.app.edonymyeon.presentation.uimodel.PostItemUiModel
+import com.domain.edonymyeon.model.Page
+import com.domain.edonymyeon.repository.SearchRepository
+import kotlinx.coroutines.launch
 
-class SearchViewModel : ViewModel()
+class SearchViewModel(private val searchRepository: SearchRepository) : ViewModel() {
+    private var page = Page()
+    private val _searchResult = MutableLiveData<List<PostItemUiModel>>()
+    val searchResult: LiveData<List<PostItemUiModel>> get() = _searchResult
+    fun getSearchResult(query: String) {
+        viewModelScope.launch {
+            searchRepository.getSearchResult(query, page.value)
+                .onSuccess { result ->
+                    _searchResult.value = searchResult.value.orEmpty() + result.map {
+                        it.toUiModel()
+                    }
+                    page = page.increasePage()
+                }
+        }
+    }
+
+    fun clearResult() {
+        page.initPage()
+        _searchResult.value = emptyList()
+    }
+}
---
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/main/search/adapter/SearchAdapter.kt
@@ -0,0 +1,21 @@
+package com.app.edonymyeon.presentation.ui.main.search.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.app.edonymyeon.presentation.ui.main.search.viewholder.SearchViewHolder
+import com.app.edonymyeon.presentation.uimodel.PostItemUiModel
+
+class SearchAdapter(private val onClick: (Long) -> Unit) :
+    ListAdapter<PostItemUiModel, SearchViewHolder>(SearchDiffUtilCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        return SearchViewHolder(parent)
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        holder.bind(this.currentList[position])
+    }
+
+    fun setPosts(posts: List<PostItemUiModel>) {
+        submitList(posts)
+    }
+}
