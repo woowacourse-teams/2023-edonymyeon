a312fb39200a8986c3ca046740e72da9f680ccee
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-08-04 00:52:06 +0900

    feat: PreferenceUtil 생성

--- android/app/src/main/java/com/app/edonymyeon/application/EdonymyeonApplication.kt
+++ android/app/src/main/java/com/app/edonymyeon/application/EdonymyeonApplication.kt
@@ -1,9 +1,12 @@
 package com.app.edonymyeon.application
 
 import android.app.Application
+import com.app.edonymyeon.data.util.PreferenceUtil
 
 class EdonymyeonApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        PreferenceUtil.init(applicationContext)
     }
 }
