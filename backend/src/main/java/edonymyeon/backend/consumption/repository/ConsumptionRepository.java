b4e56ea7ada13e8644406a2f2299f11077edde79
hectick <ch.yeondev@gmail.com>
2023-08-03 12:24:17 +0900

    refactor: 쿼리 가독성 개선

--- backend/src/main/java/edonymyeon/backend/consumption/repository/ConsumptionRepository.java
+++ backend/src/main/java/edonymyeon/backend/consumption/repository/ConsumptionRepository.java
@@ -3,6 +3,7 @@ package edonymyeon.backend.consumption.repository;
 import edonymyeon.backend.consumption.domain.Consumption;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,7 +14,11 @@ public interface ConsumptionRepository extends JpaRepository<Consumption, Long> 
 
     boolean existsByPostId(final Long postId);
 
-    @Query("SELECT c FROM Consumption c LEFT JOIN FETCH c.post p LEFT JOIN FETCH p.member m WHERE m.id = :memberId AND c.consumptionYear =:consumptionYear AND c.consumptionMonth =:consumptionMonth")
+    @EntityGraph(attributePaths = {"post"})
+    @Query("SELECT c FROM Consumption c "
+            + "WHERE c.post.member.id = :memberId "
+            + "AND c.consumptionYear =:consumptionYear "
+            + "AND c.consumptionMonth =:consumptionMonth")
     List<Consumption> findByMemberIdAndYearAndMonth(
             @Param("memberId") final Long memberId,
             @Param("consumptionYear") final Integer consumptionYear,
