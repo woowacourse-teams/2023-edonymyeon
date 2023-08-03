3c8fb6d7495589456baac0fb3b9f14d480efeabb
hectick <ch.yeondev@gmail.com>
2023-08-03 11:13:12 +0900

    docs: periodMonth에 대한 설명 추가

--- backend/src/main/java/edonymyeon/backend/consumption/application/ConsumptionService.java
+++ backend/src/main/java/edonymyeon/backend/consumption/application/ConsumptionService.java
@@ -19,6 +19,9 @@ public class ConsumptionService {
 
     private final ConsumptionRepository consumptionRepository;
 
+    /**
+     * periodMonth는 최근 몇개월간의 소비금액을 조회하고 싶은지를 나타냅니다. 예) periodMonth=6 이면 최근 6개월
+     **/
     public RecentConsumptionsResponse findRecentConsumptions(final MemberId memberId, final Integer periodMonth) {
         PeriodMonth.checkIllegalPeriodMonth(periodMonth);
 
