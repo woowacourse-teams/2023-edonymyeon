f73baf5c3102631db59d4202cdbc1c45c9aedacd
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-08-03 13:00:14 +0900

    feat: numberpicker에 년도와 달 넣는 부분 구현

--- android/app/src/main/java/com/app/edonymyeon/presentation/ui/mypost/MyPostViewModel.kt
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/mypost/MyPostViewModel.kt
@@ -3,12 +3,20 @@ package com.app.edonymyeon.presentation.ui.mypost
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.app.edonymyeon.data.common.CustomThrowable
+import com.app.edonymyeon.data.dto.response.MyPostsResponse
 import com.app.edonymyeon.mapper.toUiModel
 import com.app.edonymyeon.presentation.uimodel.ConsumptionUiModel
 import com.app.edonymyeon.presentation.uimodel.MyPostUiModel
 import com.domain.edonymyeon.model.Consumption
+import com.domain.edonymyeon.model.Date
+import com.domain.edonymyeon.model.MonthRange
 import com.domain.edonymyeon.model.MyPost
 import com.domain.edonymyeon.repository.ProfileRepository
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.YearMonth
 
 class MyPostViewModel(val repository: ProfileRepository) : ViewModel() {
 
@@ -16,15 +24,23 @@ class MyPostViewModel(val repository: ProfileRepository) : ViewModel() {
     val posts: LiveData<List<MyPostUiModel>>
         get() = _posts
 
+    private val _yearMonth = MutableLiveData<HashMap<Int, MutableList<Int>>>()
+    val yearMonth: LiveData<HashMap<Int, MutableList<Int>>>
+        get() = _yearMonth
+
+    private val _price = MutableLiveData<String>()
+    val price: LiveData<String>
+        get() = _price
+
     fun getMyPosts(size: Int, page: Int) {
-        /*viewModelScope.launch {
+        viewModelScope.launch {
             repository.getMyPosts(size, page).onSuccess {
                 it as MyPostsResponse
                 _posts.value = it.posts.map { post ->
                     post.toUiModel()
                 }
             }
-        }*/
+        }
         _posts.value = fakeMyPost.map { it.toUiModel() }
     }
 
@@ -32,50 +48,69 @@ class MyPostViewModel(val repository: ProfileRepository) : ViewModel() {
         updateConfirmConsumption(
             id,
             ConsumptionUiModel(
-                type = "PURCHASE",
+                type = PURCHASE_TYPE,
                 purchasePrice = purchasePrice,
                 year = year,
                 month = month,
             ),
         )
-        /*viewModelScope.launch {
+        viewModelScope.launch {
             repository.postPurchaseConfirm(id, purchasePrice, year, month).onSuccess {
             }.onFailure {
                 it as CustomThrowable
                 when (it.code) {
                 }
             }
-        }*/
+        }
     }
 
     fun postSavingConfirm(id: Long, year: Int, month: Int) {
         updateConfirmConsumption(
             id,
-            ConsumptionUiModel(type = "SAVING", purchasePrice = 0, year = year, month = month),
+            ConsumptionUiModel(type = SAVING_TYPE, purchasePrice = 0, year = year, month = month),
         )
-        /* viewModelScope.launch {
-             repository.postSavingConfirm(id, year, month).onSuccess { }
-                 .onFailure {
-                     it as CustomThrowable
-                     when (it.code) {
-                     }
-                 }
-         }*/
+        viewModelScope.launch {
+            repository.postSavingConfirm(id, year, month).onSuccess { }
+                .onFailure {
+                    it as CustomThrowable
+                    when (it.code) {
+                    }
+                }
+        }
     }
 
     fun deleteConfirm(id: Long) {
         updateConfirmConsumption(
             id,
-            ConsumptionUiModel(type = "NONE", purchasePrice = 0, year = 0, month = 0),
+            ConsumptionUiModel(type = NONE_TYPE, purchasePrice = 0, year = 0, month = 0),
         )
-        /* viewModelScope.launch {
-             repository.deleteConfirm(id).onSuccess {}
-                 .onFailure {
-                     it as CustomThrowable
-                     when (it.code) {
-                     }
-                 }
-         }*/
+        viewModelScope.launch {
+            repository.deleteConfirm(id).onSuccess {}
+                .onFailure {
+                    it as CustomThrowable
+                    when (it.code) {
+                    }
+                }
+        }
+    }
+
+    fun getYearMonth(id: Long) {
+        val post = _posts.value?.find { it.id == id }
+        /*val createdAt = post?.createdAt ?: ""*/
+        val createdAt = "2023-08-01T22:11:53.111371"
+        val createdYearMonth = getYearMonthFromDate(createdAt)
+        val nowYearMonth = getYearMonthFromDate(LocalDateTime.now().toString())
+        val yearMonthList = getYearMonthList(createdYearMonth, nowYearMonth)
+
+        _yearMonth.value = createYearMonthMap(yearMonthList)
+    }
+
+    fun setPurchasePriceTextChanged(price: CharSequence, start: Int, before: Int, count: Int) {
+        _price.value = price.toString()
+    }
+
+    fun setPurchasePrice(price: String) {
+        _price.value = price
     }
 
     private fun updateConfirmConsumption(postId: Long, consumption: ConsumptionUiModel) {
@@ -86,6 +121,23 @@ class MyPostViewModel(val repository: ProfileRepository) : ViewModel() {
                 post
             }
         }
+    }
+
+    private fun getYearMonthFromDate(dateString: String): YearMonth {
+        return Date(dateString).yearMonth
+    }
+
+    private fun getYearMonthList(start: YearMonth, end: YearMonth): List<String> {
+        return MonthRange(start, end).yearMonthList
+    }
+
+    private fun createYearMonthMap(yearMonthList: List<String>): HashMap<Int, MutableList<Int>> {
+        val map = hashMapOf<Int, MutableList<Int>>()
+        yearMonthList.forEach { yearMonth ->
+            val (year, month) = yearMonth.split("-").map { it.toInt() }
+            map.computeIfAbsent(year) { mutableListOf() }.add(month)
+        }
+        return map
     }
 
     private val fakeMyPost = listOf(
@@ -129,4 +181,10 @@ class MyPostViewModel(val repository: ProfileRepository) : ViewModel() {
             ),
         ),
     )
+
+    companion object {
+        private const val PURCHASE_TYPE = "PURCHASE"
+        private const val SAVING_TYPE = "SAVING"
+        private const val NONE_TYPE = "NONE"
+    }
 }
--- android/app/src/main/java/com/app/edonymyeon/presentation/ui/mypost/dialog/ConsumptionDialog.kt
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/mypost/dialog/ConsumptionDialog.kt
@@ -7,9 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import app.edonymyeon.R
 import app.edonymyeon.databinding.DialogInputConsumptionBinding
 import com.app.edonymyeon.presentation.ui.mypost.ConsumptionType
 import com.app.edonymyeon.presentation.ui.mypost.MyPostViewModel
+import com.app.edonymyeon.presentation.util.makeSnackbar
 
 class ConsumptionDialog(
     private val type: ConsumptionType,
@@ -31,8 +33,25 @@ class ConsumptionDialog(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        binding.type = type
+        initBinding()
+        initObserve()
+        setNumberPicker()
         initListener()
+    }
+
+    private fun initBinding() {
+        binding.type = type
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+    }
+
+    private fun initObserve() {
+        viewModel.price.observe(this) { price ->
+            runCatching { if (price != BLANK) price?.toInt() ?: 0 }.onFailure {
+                binding.root.makeSnackbar(this.getString(R.string.dialog_input_price_error_message))
+                viewModel.setPurchasePrice(BLANK)
+            }
+        }
     }
 
     private fun initListener() {
@@ -41,7 +60,7 @@ class ConsumptionDialog(
             when (type) {
                 ConsumptionType.PURCHASE -> viewModel.postPurchaseConfirm(
                     id,
-                    binding.etConsumptionPrice.toString().toInt(),
+                    binding.etConsumptionPrice.text.toString().toInt(),
                     binding.npYear.value,
                     binding.npMonth.value,
                 )
@@ -53,5 +72,36 @@ class ConsumptionDialog(
             }
             dismiss()
         }
+        binding.npYear.setOnValueChangedListener { _, _, newVal ->
+            updateMonthNumberPicker(newVal)
+        }
+    }
+
+    private fun setNumberPicker() {
+        viewModel.getYearMonth(id)
+        val minYear = viewModel.yearMonth.value?.keys?.min() ?: 0
+        val maxYear = viewModel.yearMonth.value?.keys?.max() ?: 0
+
+        updateYearNumberPicker(minYear, maxYear)
+        updateMonthNumberPicker(minYear)
+    }
+
+    private fun updateYearNumberPicker(minYear: Int, maxYear: Int) {
+        binding.npYear.minValue = minYear
+        binding.npYear.maxValue = maxYear
+        binding.npYear.value = minYear
+    }
+
+    private fun updateMonthNumberPicker(newVal: Int) {
+        val month = viewModel.yearMonth.value?.get(newVal)?.toTypedArray() ?: emptyArray()
+
+        binding.npMonth.minValue = month.min()
+        binding.npMonth.maxValue = month.max()
+        binding.npMonth.value = month.min()
+        binding.npMonth.wrapSelectorWheel = false
+    }
+
+    companion object {
+        private const val BLANK = ""
     }
 }
