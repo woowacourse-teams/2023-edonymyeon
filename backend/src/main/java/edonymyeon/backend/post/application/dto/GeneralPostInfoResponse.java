0d9c711f37329f012a26366fef25eb79c62d1461
BackFoxx <backfoxxx@gmail.com>
2023-08-04 11:24:20 +0900

    fix: 서비스에서 응답하는 dto에 조회수가 반영되지 않는 문제 해결

--- backend/src/main/java/edonymyeon/backend/post/application/dto/GeneralPostInfoResponse.java
+++ backend/src/main/java/edonymyeon/backend/post/application/dto/GeneralPostInfoResponse.java
@@ -21,8 +21,7 @@ public record GeneralPostInfoResponse(
                 post.getContent(),
                 new WriterResponse(post.getMember().getNickname()),
                 post.getCreatedAt(),
-                new ReactionCountResponse(0, 0, 0)
-                // TODO: 조회수
+                new ReactionCountResponse(post.getViewCount(), 0, 0)
                 // TODO: 스크랩 수
                 // TODO: 댓글 수
         );
