d1b89099196a2a125a48f822fa6e1b495732899f
Sujin Kim <sujinkim919@gmail.com>
2023-08-09 05:22:09 +0900

    feat: snackbar 커스텀 디자인 및 애니메이션 추가

--- android/app/src/main/java/com/app/edonymyeon/presentation/util/ActivityUtil.kt
+++ android/app/src/main/java/com/app/edonymyeon/presentation/util/ActivityUtil.kt
@@ -1,15 +1,17 @@
 package com.app.edonymyeon.presentation.util
 
 import android.view.View
+import com.google.android.material.snackbar.BaseTransientBottomBar.ANIMATION_MODE_SLIDE
 import com.google.android.material.snackbar.Snackbar
 
 fun View.makeSnackbar(message: String) {
-    Snackbar.make(this, message, Snackbar.LENGTH_SHORT).show()
+    Snackbar.make(this, message, Snackbar.LENGTH_SHORT)
+        .setAnimationMode(ANIMATION_MODE_SLIDE).show()
 }
 
 fun View.makeSnackbarWithEvent(message: String, eventTitle: String, event: () -> Unit) {
     Snackbar.make(this, message, Snackbar.LENGTH_SHORT)
         .setAction(eventTitle) {
             event()
-        }.show()
+        }.setAnimationMode(ANIMATION_MODE_SLIDE).show()
 }
