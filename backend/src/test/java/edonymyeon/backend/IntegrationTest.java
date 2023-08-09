177a907b9b710e51b9d3ff933e3aaeebce67c783
BackFoxx <backfoxxx@gmail.com>
2023-08-09 16:25:26 +0900

    feat: 공통된 Fixture 메소드를 IntegrationTest로 이동

--- backend/src/test/java/edonymyeon/backend/IntegrationTest.java
+++ backend/src/test/java/edonymyeon/backend/IntegrationTest.java
@@ -1,5 +1,6 @@
 package edonymyeon.backend;
 
+import edonymyeon.backend.member.domain.Member;
 import edonymyeon.backend.support.ConsumptionTestSupport;
 import edonymyeon.backend.support.MemberTestSupport;
 import edonymyeon.backend.support.PostImageInfoTestSupport;
@@ -7,6 +8,9 @@ import edonymyeon.backend.support.PostIntegrationTestSupport;
 import edonymyeon.backend.support.PostTestSupport;
 import edonymyeon.backend.support.ProfileImageInfoTestSupport;
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.io.File;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -19,6 +23,9 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
+
+    protected static final File 이미지1 = new File("./src/test/resources/static/img/file/test_image_1.jpg");
+    protected static final File 이미지2 = new File("./src/test/resources/static/img/file/test_image_2.jpg");
 
     @Autowired
     protected ProfileImageInfoTestSupport profileImageInfoTestSupport;
@@ -44,6 +51,37 @@ public class IntegrationTest {
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+    }
+
+    protected Member 사용자를_하나_만든다() {
+        return memberTestSupport.builder().build();
+    }
+
+    protected ExtractableResponse<Response> 게시글을_하나_만든다(final Member member) {
+        return postIntegrationTestSupport.builder()
+                .member(member)
+                .title("this is title")
+                .content("this is content")
+                .price(1000L)
+                .image1(이미지1)
+                .image2(이미지2)
+                .build();
+    }
+
+    protected long 응답의_location헤더에서_id를_추출한다(final ExtractableResponse<Response> 게시글_생성_요청_결과) {
+        final String location = 게시글_생성_요청_결과.header("location");
+        return Long.parseLong(location.split("/")[2]);
+    }
+
+
+    protected ExtractableResponse<Response> 게시글_하나를_상세_조회한다(final Member 열람인, final long 게시글_id) {
+        return RestAssured
+                .given()
+                .when()
+                .auth().preemptive().basic(열람인.getEmail(), 열람인.getPassword())
+                .get("/posts/" + 게시글_id)
+                .then()
+                .extract();
     }
 }
 
