0bf01576d78f1624e1f99d39ea8e12b1eea64831
BackFoxx <backfoxxx@gmail.com>
2023-08-09 17:38:29 +0900

    feat: 신고 타입 설정

--- backend/src/main/java/edonymyeon/backend/report/domain/AbusingType.java
+++ backend/src/main/java/edonymyeon/backend/report/domain/AbusingType.java
@@ -5,7 +5,12 @@ import edonymyeon.backend.global.exception.ExceptionInformation;
 import java.util.Arrays;
 
 public enum AbusingType {
-    DDONG(4);
+    COMMERCIAL_PURPOSE(1),
+    PERSONAL_INFORMATION_DISCLOSURE(2),
+    OBSCENITY(3),
+    PROFANITY(4),
+    REPETITIVE_CONTENT(5),
+    ETC(6);
 
     private final int typeCode;
 
