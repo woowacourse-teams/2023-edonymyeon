955ea7d0b6ceff5241e630a8cebb42f8f418e4cd
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-08-09 19:22:08 +0900

    chore: versionCode update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.edonymyeon"
         minSdk = 28
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
