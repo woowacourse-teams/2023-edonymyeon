ecf3a4f26a0c0d4f0cf2c76908ab9950f3a78e23
jyeost <pig9619@naver.com>
2023-08-10 18:07:04 +0900

    test: findHotPosts 메서드에 대한 테스트 케이스를 추가함

--- backend/src/test/java/edonymyeon/backend/report/ReportServiceTest.java
+++ backend/src/test/java/edonymyeon/backend/report/ReportServiceTest.java
@@ -15,9 +15,11 @@ import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
+@Transactional
 @SpringBootTest
 class ReportServiceTest {
 
