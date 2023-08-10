ef705089104b245a57ab3b719422e3df0c9e3192
jyeost <pig9619@naver.com>
2023-08-10 21:28:25 +0900

    docs: 검색파트 문서화

--- backend/src/test/java/edonymyeon/backend/post/docs/PostControllerDocsTest.java
+++ backend/src/test/java/edonymyeon/backend/post/docs/PostControllerDocsTest.java
@@ -1,25 +1,5 @@
 package edonymyeon.backend.post.docs;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import edonymyeon.backend.image.postimage.domain.PostImageInfo;
 import edonymyeon.backend.image.postimage.domain.PostImageInfos;
 import edonymyeon.backend.image.postimage.repository.PostImageInfoRepository;
@@ -27,16 +7,13 @@ import edonymyeon.backend.member.application.dto.ActiveMemberId;
 import edonymyeon.backend.member.domain.Member;
 import edonymyeon.backend.member.repository.MemberRepository;
 import edonymyeon.backend.post.ImageFileCleaner;
+import edonymyeon.backend.post.application.GeneralFindingCondition;
 import edonymyeon.backend.post.application.dto.AllThumbsInPostResponse;
 import edonymyeon.backend.post.application.dto.ThumbsStatusInPostResponse;
 import edonymyeon.backend.post.domain.Post;
 import edonymyeon.backend.post.repository.PostRepository;
 import edonymyeon.backend.thumbs.application.PostThumbsServiceImpl;
 import jakarta.servlet.http.Part;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 import org.apache.http.entity.ContentType;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -46,15 +23,38 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.mock.web.MockPart;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(ReplaceUnderscores.class)
@@ -88,6 +88,10 @@ public class PostControllerDocsTest implements ImageFileCleaner {
         when(postRepository.findById(게시글.getId())).thenReturn(Optional.of(게시글));
         doNothing().when(postRepository).deleteById(게시글.getId());
         when(postRepository.findAllBy(any())).thenReturn(new SliceImpl<>(List.of(게시글)));
+    }
+
+    private void 게시글_검색을_모킹한다(final Post 게시글) {
+        when(postRepository.findAll((Specification<Post>) any(), (Pageable) any())).thenReturn(new PageImpl<>(List.of(게시글)));
     }
 
     private void 게시글_이미지_정보_레포지토리를_모킹한다() {
@@ -218,6 +222,56 @@ public class PostControllerDocsTest implements ImageFileCleaner {
         );
 
         this.mockMvc.perform(게시글_전체_조회_요청)
+                .andExpect(status().isOk())
+                .andDo(문서화);
+    }
+
+    @Test
+    void 게시글을_검색한다() throws Exception {
+        final GeneralFindingCondition findingCondition = GeneralFindingCondition.builder().build();
+
+        final Member 글쓴이 = new Member(1L, "email", "password", "nickname", null);
+        final Post 게시글 = new Post(1L, "햄버거 먹어도 되나요", "불고기 버거 세일중이던데", 1000L, 글쓴이, PostImageInfos.create(), LocalDateTime.now(), 0);
+
+        회원_레포지토리를_모킹한다(글쓴이);
+        게시글_레포지토리를_모킹한다(게시글);
+        게시글_검색을_모킹한다(게시글);
+
+        final var 게시글_검색_요청 = get("/search")
+                .queryParam("query", "세일 햄버거")
+                .queryParam("page", findingCondition.getPage().toString())
+                .queryParam("size", findingCondition.getSize().toString())
+                .queryParam("sort-by", findingCondition.getSortBy().getName())
+                .queryParam("sort-direction", findingCondition.getSortDirection().name())
+                .contentType(MediaType.APPLICATION_JSON_VALUE);
+
+        FieldDescriptor[] 응답 = {
+                fieldWithPath("content").description("게시글 목록"),
+                fieldWithPath("content[].id").description("게시글 id"),
+                fieldWithPath("content[].title").description("게시글 제목"),
+                fieldWithPath("content[].image").description("게시글 이미지 url"),
+                fieldWithPath("content[].content").description("게시글 내용"),
+                fieldWithPath("content[].createdAt").description("게시글 생성 날짜"),
+                fieldWithPath("content[].writer.nickName").description("닉네임"),
+                fieldWithPath("content[].reactionCount.viewCount").description("조회수"),
+                fieldWithPath("content[].reactionCount.commentCount").description("댓글 수"),
+                fieldWithPath("content[].reactionCount.scrapCount").description("스크랩수"),
+                fieldWithPath("last").description("현재 요청한 페이지가 마지막 페이지인지")
+        };
+
+        final RestDocumentationResultHandler 문서화 = document("post-search",
+                preprocessResponse(prettyPrint()),
+                queryParameters(
+                        parameterWithName("size").description("한 페이지 당 조회할 게시글 수"),
+                        parameterWithName("page").description("조회할 게시글의 페이지"),
+                        parameterWithName("query").description("검색하는 단어"),
+                        parameterWithName("sort-by").description("정렬 기준"),
+                        parameterWithName("sort-direction").description("정렬 방향")
+                ),
+                responseFields(응답)
+        );
+
+        this.mockMvc.perform(게시글_검색_요청)
                 .andExpect(status().isOk())
                 .andDo(문서화);
     }
