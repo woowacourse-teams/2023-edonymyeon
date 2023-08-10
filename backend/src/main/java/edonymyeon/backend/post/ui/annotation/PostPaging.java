baeb0950a23f9f3d918a18eb43ab960d403a7638
jyeost <pig9619@naver.com>
2023-08-10 17:13:09 +0900

    docs: java doc으로 클래스와 메서드에 대한 설명을 각각 적어서 설명한다

--- backend/src/main/java/edonymyeon/backend/post/ui/annotation/PostPaging.java
+++ backend/src/main/java/edonymyeon/backend/post/ui/annotation/PostPaging.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 보통의 게시글 목록을 조회하는 정책
+ * page, size, sort 기준, sort 순서 조건을 입력 받는다
+ */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PostPaging {
