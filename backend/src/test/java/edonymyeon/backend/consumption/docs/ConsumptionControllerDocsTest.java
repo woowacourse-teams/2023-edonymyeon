2f1c391c12d0d58fc1f92fd2da20b7c4777df4cd
hectick <ch.yeondev@gmail.com>
2023-08-10 13:31:43 +0900

    docs: 구매확정, 절약확정, 확정취소 문서화

---
+++ backend/src/test/java/edonymyeon/backend/consumption/docs/ConsumptionControllerDocsTest.java
@@ -0,0 +1,13 @@
+package edonymyeon.backend.consumption.docs;
+
+import edonymyeon.backend.DocsTest;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class ConsumptionControllerDocsTest extends DocsTest {
+
+    @Test
+    void 특정기간의_소비금액을_확인한다() {
+
+    }
+}
