3d5a8cd65e4927b14847d796ef7882967297f2c6
Sujin Kim <sujinkim919@gmail.com>
2023-08-11 16:38:08 +0900

    Feat/#236 로그인 여부 기능 접근 (#243)
    * feat: 로그인 안했을 때 내가 쓴 글을 누르면 로그인 페이지로 이동
    
    * feat: 로그인 안했을 때 글 작성 버튼을 누르면 로그인 페이지로 이동
    
    * refactor: 함수명 통일 및 함수 선언 순서를 호출 순서로 변경
    
    * refactor: 로그인 체크 관련 로직 통일화
    
    * feat: 로그인 안했을 때 알람 페이지로 이동하면 로그인 안내 문구 뜨도록 구현
    
    * feat: 로그인 안했을 때 로그아웃 버튼 안보이도록 구현
    
    * refactor: 로그인 안했을 때 live data 초기화 함수화
    
    - string값을 resource로 관리한 후, 이를 뷰에서 전달하는 방향으로 수정하기 위함
    
    * feat: 로그인하고 돌아온 후 뷰가 즉각 반영되도록 디테일 추가
    
    - 로그인을 하면 로그아웃 버튼이 안 보이게 된다
    - 내가 쓴 글, 회원정보 수정에 대한 이벤트가 달라진다
    
    * refactor: 레이아웃 파일 수정
    
    - theme을 MaterialComponents로 변경 (스낵바 커스텀을 위해)
    
    - 그에 따라 버튼들을 AppCompatButton으로 수정
    
    - 그에 따라 상단 statusBar 색상 수정
    
    - ImageButton 배경 불투명 개선
    
    - 배경 색이 하단에 잘리는 문제 개선
    
    * feat: snackbar 커스텀 디자인 및 애니메이션 추가
    
    * refactor: token이 없을 때 기본값을 ""가 아니라 null로 수정
    
    * refactor: ActivityUtil을 ViewUtil로 수정
    
    * refactor: 로그인 여부에 대한 변수를 viewModel에 선언
    
    * fix: 로그인하고 그래프가 보이지 않는 오류 수정
    
    * refactor: 그래프 그리기 관련 성능 개선
    
    * feat: 데이터가 없을 때 안내 문구 및 색상 수정
    
    * refactor: 함수명 통일 및 함수 선언 순으로 정렬

--- android/app/src/main/java/com/app/edonymyeon/presentation/ui/post/PostActivity.kt
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/post/PostActivity.kt
@@ -11,9 +11,11 @@ import app.edonymyeon.R
 import app.edonymyeon.databinding.ActivityPostBinding
 import com.app.edonymyeon.data.datasource.post.PostRemoteDataSource
 import com.app.edonymyeon.data.repository.PostRepositoryImpl
+import com.app.edonymyeon.presentation.ui.login.LoginActivity
 import com.app.edonymyeon.presentation.ui.post.adapter.PostAdapter
 import com.app.edonymyeon.presentation.ui.postdetail.PostDetailActivity
 import com.app.edonymyeon.presentation.ui.posteditor.PostEditorActivity
+import com.app.edonymyeon.presentation.util.makeSnackbarWithEvent
 
 class PostActivity : AppCompatActivity() {
     private val activityLauncher =
@@ -36,14 +38,25 @@ class PostActivity : AppCompatActivity() {
         setContentView(binding.root)
         initAppbar()
         setPostAdapter()
-        setClickListener()
+        setListener()
         loadNewData()
     }
 
-    private fun setClickListener() {
-        binding.ivPostNew.setOnClickListener {
-            startPostEditorActivity()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun initAppbar() {
+        setSupportActionBar(binding.tbPost)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(R.string.post_all_post)
     }
 
     private fun setPostAdapter() {
@@ -74,32 +87,30 @@ class PostActivity : AppCompatActivity() {
         })
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                finish()
-                true
-            }
-
-            else -> {
-                false
+    private fun setListener() {
+        binding.ivPostNew.setOnClickListener {
+            if (viewModel.isLogin) {
+                navigateToPostEditor()
+            } else {
+                binding.root.makeSnackbarWithEvent(
+                    message = getString(R.string.all_required_login),
+                    eventTitle = getString(R.string.login_title),
+                ) { navigateToLogin() }
             }
         }
-    }
-
-    private fun startPostEditorActivity() {
-        activityLauncher.launch(PostEditorActivity.newIntent(this, PostEditorActivity.POST_CODE))
-    }
-
-    private fun initAppbar() {
-        setSupportActionBar(binding.tbPost)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = getString(R.string.post_all_post)
     }
 
     private fun loadNewData() {
         viewModel.clearResult()
         viewModel.getPosts()
+    }
+
+    private fun navigateToPostEditor() {
+        activityLauncher.launch(PostEditorActivity.newIntent(this, PostEditorActivity.POST_CODE))
+    }
+
+    private fun navigateToLogin() {
+        startActivity(LoginActivity.newIntent(this))
     }
 
     companion object {
