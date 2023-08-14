9ef5dd61131199ca3b8d8f4216cf3be4ce8be58d
김지현 <pig9619@naver.com>
2023-08-14 21:38:19 +0900

    fix: 캐시테스트를 테스트 컨테이너를 띄우지 않고 직접 하도록 한다 (#288)
    * feat: 테스트 컨테이너를 작동하지 않도록 수정하고 ci에서 직접 레디스를 설치하고 사용하는지 확인한다
    
    * docs: ci가 제대로 작동함을 확인하고 주석을 삭제함
    
    * Revert "docs: ci가 제대로 작동함을 확인하고 주석을 삭제함"
    
    This reverts commit 4ecf5aaa3930f320ed8451439e2d37446553589f.
    
    * docs: ci가 제대로 작동함을 확인하고 주석을 삭제함

--- backend/src/test/java/edonymyeon/backend/CacheConfig.java
+++ backend/src/test/java/edonymyeon/backend/CacheConfig.java
@@ -4,8 +4,6 @@ import edonymyeon.backend.cache.util.HotPostCachePolicy;
 import edonymyeon.backend.post.application.TestCacheKeyStrategy;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
 public class CacheConfig {
@@ -13,20 +11,5 @@ public class CacheConfig {
     @Bean
     public HotPostCachePolicy hotPostCachePolicy(){
         return new HotPostCachePolicy(new TestCacheKeyStrategy());
-    }
-
-    // test container 설정
-    private static final String REDIS_DOCKER_IMAGE = "redis:5.0.3-alpine";
-
-    static {
-        GenericContainer<?> REDIS_CONTAINER =
-                new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
-                        .withExposedPorts(6379)
-                        .withReuse(true);
-
-        REDIS_CONTAINER.start();
-
-        System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
-        System.setProperty("spring.data.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
     }
 }
