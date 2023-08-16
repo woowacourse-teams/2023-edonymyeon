d90beb88b728f398dd800f51bf727e79df58f4a9
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-08-17 00:01:13 +0900

    style: ktlint 적용하여 포맷팅

--- android/app/src/main/java/com/app/edonymyeon/presentation/ui/mypost/MyPostViewModelFactory.kt
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/mypost/MyPostViewModelFactory.kt
@@ -5,7 +5,10 @@ import androidx.lifecycle.ViewModelProvider
 import com.domain.edonymyeon.repository.PostRepository
 import com.domain.edonymyeon.repository.ProfileRepository
 
-class MyPostViewModelFactory(private val profileRepository: ProfileRepository, private val postRepository: PostRepository) :
+class MyPostViewModelFactory(
+    private val profileRepository: ProfileRepository,
+    private val postRepository: PostRepository,
+) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return MyPostViewModel(profileRepository, postRepository) as T
