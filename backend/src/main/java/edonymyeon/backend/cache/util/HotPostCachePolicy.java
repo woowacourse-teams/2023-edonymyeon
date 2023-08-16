d5801be6bb64370ced28e6e9a66a451d903b7c7f
김지현 <pig9619@naver.com>
2023-08-16 20:10:42 +0900

    Fix/#304 레디스 캐싱과 파싱오류 수정 (#306)
    * refactor: redis 파싱 타입을 직접 해주지 않기 위해서 설정 변경
    
    * feat: redis에 직접 값을 저장하던 것을 CrudRepository를 사용하여 JPA처럼 객체를 저장하도록 수정함
    
    * refactor: 리뷰 반영하여 수정
    - 코드 포맷
    - 예외 처리

--- backend/src/main/java/edonymyeon/backend/cache/util/HotPostCachePolicy.java
+++ backend/src/main/java/edonymyeon/backend/cache/util/HotPostCachePolicy.java
@@ -10,12 +10,8 @@ public class HotPostCachePolicy {
 
     private final CacheKeyStrategy cacheKeyStrategy;
 
-    public String getPostIdsCacheKey(HotFindingCondition hotFindingCondition){
-        return cacheKeyStrategy.getPostIdsCacheKey(hotFindingCondition.getSize(), hotFindingCondition.getPage());
-    }
-
-    public String getLastCacheKey(HotFindingCondition hotFindingCondition){
-        return cacheKeyStrategy.getLastCacheKey(hotFindingCondition.getSize(), hotFindingCondition.getPage());
+    public String getKey(HotFindingCondition hotFindingCondition){
+        return cacheKeyStrategy.geCacheKey(hotFindingCondition.getSize(), hotFindingCondition.getPage());
     }
 
     public int getExpiredSeconds() {
