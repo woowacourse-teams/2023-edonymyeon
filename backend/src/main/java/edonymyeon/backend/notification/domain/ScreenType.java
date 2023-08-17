65928de659e8a923aab24901a9665c82d20341fb
BackFoxx <backfoxxx@gmail.com>
2023-08-17 14:04:01 +0900

    refactor: ScreenType의 의미를 javadoc으로 설명

--- backend/src/main/java/edonymyeon/backend/notification/domain/ScreenType.java
+++ backend/src/main/java/edonymyeon/backend/notification/domain/ScreenType.java
@@ -1,6 +1,16 @@
 package edonymyeon.backend.notification.domain;
 
+/**
+ * 알림을 통해 접속할 페이지의 종류를 나타내는 enum입니다.
+ */
 public enum ScreenType {
+    /**
+     * 알림을 클릭할 경우 특정 게시글의 상세 페이지로 이동합니다.
+     */
+    POST,
 
-    POST, MYPOST
+    /**
+     * 알림을 클릭할 경우 내가 쓴 글 페이지로 이동합니다.
+     */
+    MYPOST
 }
