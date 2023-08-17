55147352ee2901d3b3a75c5c395cb3edda92547b
Sujin Kim <sujinkim919@gmail.com>
2023-08-17 14:18:19 +0900

    fix: 충돌 해결

--- android/app/src/main/java/com/app/edonymyeon/application/EdonymyeonApplication.kt
+++ android/app/src/main/java/com/app/edonymyeon/application/EdonymyeonApplication.kt
@@ -8,18 +8,6 @@ import com.app.edonymyeon.data.service.client.RetrofitClient
 import com.app.edonymyeon.data.util.PreferenceUtil
 import com.kakao.sdk.common.KakaoSdk
 
-<<<<<<< HEAD
-import app.edonymyeon.R
-
-=======
-import androidx.appcompat.app.AppCompatDelegate
-
->>>>>>> 0e9d5bc8a849f92eacf65f6d921aa7b4294a08dc
-import com.app.edonymyeon.data.datasource.auth.AuthLocalDataSource
-import com.app.edonymyeon.data.service.client.RetrofitClient
-import com.app.edonymyeon.data.util.PreferenceUtil
-import com.kakao.sdk.common.KakaoSdk
-
 class EdonymyeonApplication : Application() {
     override fun onCreate() {
         super.onCreate()
