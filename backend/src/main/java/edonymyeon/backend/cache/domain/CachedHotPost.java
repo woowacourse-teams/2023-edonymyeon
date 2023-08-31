fe90f06296c3b25ac94205cc487cc1df7f5746b8
jyeost <pig9619@naver.com>
2023-08-31 18:54:41 +0900

    fix: 캐싱된 list에 null처리 추가

--- backend/src/main/java/edonymyeon/backend/cache/domain/CachedHotPost.java
+++ backend/src/main/java/edonymyeon/backend/cache/domain/CachedHotPost.java
@@ -7,7 +7,10 @@ import org.springframework.data.redis.core.RedisHash;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 @Getter
@@ -30,9 +33,24 @@ public class CachedHotPost {
     }
 
     public void refreshData(final List<Long> hotPostIds, final boolean isLast) {
-        this.postIds.clear();
+        initializePostIds();
         this.postIds.addAll(hotPostIds);
         this.isLast = isLast;
         refreshTime = LocalDateTime.now();
+    }
+
+    private void initializePostIds() {
+        if (Objects.nonNull(this.postIds)) {
+            this.postIds.clear();
+            return;
+        }
+        this.postIds = new ArrayList<>();
+    }
+
+    public List<Long> getPostIds() {
+        if(Objects.isNull(this.postIds)){
+            return Collections.emptyList();
+        }
+        return this.postIds;
     }
 }
