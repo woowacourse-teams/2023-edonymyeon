b006618ce8829ac1fc7bd33d8b216115421a9eb8
jyeost <pig9619@naver.com>
2023-08-31 18:52:23 +0900

    refactor: domain 디렉토리 루트 수정

--- backend/src/test/java/edonymyeon/backend/post/application/PostServiceHotPostsTest.java
+++ backend/src/test/java/edonymyeon/backend/post/application/PostServiceHotPostsTest.java
@@ -5,6 +5,7 @@ import edonymyeon.backend.cache.application.HotPostsRedisRepository;
 import edonymyeon.backend.cache.application.PostCachingService;
 import edonymyeon.backend.cache.util.HotPostCachePolicy;
 import edonymyeon.backend.post.application.dto.GeneralPostInfoResponse;
+import edonymyeon.backend.post.domain.Post;
 import edonymyeon.backend.post.repository.PostRepository;
 import edonymyeon.backend.support.IntegrationTest;
 import edonymyeon.backend.support.PostTestSupport;
