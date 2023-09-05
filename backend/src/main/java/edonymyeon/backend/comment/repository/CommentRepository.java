c27c6331ac14b7043541c57832a786ecde5282bd
Sung Chaeyeon <ch.yeondev@gmail.com>
2023-09-05 21:36:03 +0900

    Feat/#372 댓글만 조회하는 API 구현 (#373)
    * feat: 응답 dto 클래스 생성
    
    * feat: 댓글만 조회 api 구현
    
    * test: 댓글만 조회 테스트 작성
    
    * docs: rest docs 문서화
    
    * refactor: 파일의 urL을 만들어주는 부분을 캡슐화
    
    * feat: 댓글 수도 같이 조회할 수 있도록 api 수정
    
    * fix: 반환할 이미지가 없을 때 예외가 발생하는 버그 수정
    
    * refactor: primitive 타입으로 수정
    
    * refactor: 메서드 네이밍 수정
    
    * feat: 게시글이 존재하는지 확인하는 기능 추가
    - 게시글 상세페이지에서 게시글이 삭제된 경우 빈 리스트가 반환되어 버리면 댓글이 모두 증발해버려 사용자가 깜짝 놀랄 수 있기 때문에 게시글이 삭제되었다는 예외코드와 메세지를 반환한다.

--- backend/src/main/java/edonymyeon/backend/comment/repository/CommentRepository.java
+++ backend/src/main/java/edonymyeon/backend/comment/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package edonymyeon.backend.comment.repository;
 
 import edonymyeon.backend.comment.domain.Comment;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @EntityGraph(attributePaths = {"member", "commentImageInfo"})
     Optional<Comment> findByIdAndPostId(final Long id, final Long postId);
+
+    @EntityGraph(attributePaths = {"post", "member", "commentImageInfo"})
+    List<Comment> findAllByPostId(final Long postId);
 }
--- backend/src/main/java/edonymyeon/backend/image/domain/Domain.java
+++ backend/src/main/java/edonymyeon/backend/image/domain/Domain.java
@@ -31,4 +31,8 @@ public class Domain {
             throw new EdonymyeonException(IMAGE_DOMAIN_INVALID);
         }
     }
+
+    public String convertToImageUrl(final String fileName) {
+        return domain + fileName;
+    }
 }
