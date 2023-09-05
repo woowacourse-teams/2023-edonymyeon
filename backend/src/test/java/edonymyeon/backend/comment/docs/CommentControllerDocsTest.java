63a244a5592ab0f65bbc164423493003c871bf27
Sung Chaeyeon <ch.yeondev@gmail.com>
2023-09-05 21:36:03 +0900

    Feat/#372 댓글만 조회하는 API 구현 (#373)
    * feat: 응답 dto 클래스 생성
    
    * feat: 댓글만 조회 api 구현
    
    * test: 댓글만 조회 테스트 작성
    
    * docs: rest docs 문서화
    
    * refactor: 파일의 urL을 만들어주는 부분을 캡슐화
    
    * feat: 댓글 수도 같이 조회할 수 있도록 api 수정
    
    * fix: 반환할 이미지가 없을 때 예외가 발생하는 버그 수정
    
    * refactor: primitive 타입으로 수정
    
    * refactor: 메서드 네이밍 수정
    
    * feat: 게시글이 존재하는지 확인하는 기능 추가
    - 게시글 상세페이지에서 게시글이 삭제된 경우 빈 리스트가 반환되어 버리면 댓글이 모두 증발해버려 사용자가 깜짝 놀랄 수 있기 때문에 게시글이 삭제되었다는 예외코드와 메세지를 반환한다.

--- backend/src/test/java/edonymyeon/backend/comment/docs/CommentControllerDocsTest.java
+++ backend/src/test/java/edonymyeon/backend/comment/docs/CommentControllerDocsTest.java
@@ -8,10 +8,13 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.headerWit
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
@@ -20,11 +23,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import edonymyeon.backend.comment.application.CommentService;
+import edonymyeon.backend.comment.application.dto.response.CommentDto;
+import edonymyeon.backend.comment.application.dto.response.CommentsResponse;
+import edonymyeon.backend.comment.application.dto.response.WriterDto;
 import edonymyeon.backend.member.domain.Member;
 import edonymyeon.backend.support.IntegrationTest;
-import edonymyeon.backend.support.MemberTestSupport;
+import edonymyeon.backend.support.TestMemberBuilder;
 import jakarta.servlet.http.Part;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.entity.ContentType;
 import org.junit.jupiter.api.Test;
@@ -51,7 +59,7 @@ public class CommentControllerDocsTest {
     @MockBean
     private final CommentService commentService;
 
-    private final MemberTestSupport memberTestSupport;
+    private final TestMemberBuilder memberTestSupport;
 
     @Test
     void 댓글을_작성한다() throws Exception {
@@ -114,6 +122,64 @@ public class CommentControllerDocsTest {
 
         mockMvc.perform(댓글_삭제_요청)
                 .andExpect(status().isNoContent())
+                .andDo(문서화);
+    }
+
+    @Test
+    void 댓글을_조회한다() throws Exception {
+        final CommentsResponse commentsResponse = new CommentsResponse(
+                3,
+                List.of(
+                        new CommentDto(
+                                4L,
+                                null,
+                                "그건 너무 비싸용",
+                                false,
+                                LocalDateTime.now(),
+                                new WriterDto("훈수쟁이")
+                        ),
+                        new CommentDto(
+                                8L,
+                                "http://edonymyeon.site/images/image2.png",
+                                "그돈이면 치킨이 세마리",
+                                false,
+                                LocalDateTime.now(),
+                                new WriterDto("훈수충")
+                        ),
+                        new CommentDto(
+                                9L,
+                                "http://edonymyeon.site/images/image3.png",
+                                "배고프다",
+                                false,
+                                LocalDateTime.now(),
+                                new WriterDto("거지")
+                        )
+                )
+        );
+        when(commentService.findCommentsByPostId(any(), anyLong())).thenReturn(commentsResponse);
+
+        final MockHttpServletRequestBuilder 댓글_조회_요청 = get("/posts/{postId}/comments", 1L)
+                .contentType(MediaType.APPLICATION_JSON_VALUE);
+
+        final RestDocumentationResultHandler 문서화 = document("comment-findAllByPost",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                pathParameters(
+                        parameterWithName("postId").description("게시글 id")
+                ),
+                responseFields(
+                        fieldWithPath("commentCount").description("댓글 수"),
+                        fieldWithPath("comments[].id").description("댓글 id"),
+                        fieldWithPath("comments[].image").description("댓글에 첨부된 사진의 url").optional(),
+                        fieldWithPath("comments[].comment").description("댓글 본문"),
+                        fieldWithPath("comments[].isWriter").description("댓글 조회 요청을 보낸 사용자가 댓글 작성자 본인인지 여부"),
+                        fieldWithPath("comments[].createdAt").description("댓글 작성 시각"),
+                        fieldWithPath("comments[].writer.nickname").description("댓글 작성자의 닉네임")
+                )
+        );
+
+        mockMvc.perform(댓글_조회_요청)
+                .andExpect(status().isOk())
                 .andDo(문서화);
     }
 }
