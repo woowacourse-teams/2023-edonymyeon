976251ea88bd145c52a24db1ceaac8c1c8e3c5f5
BackFoxx <backfoxxx@gmail.com>
2023-09-06 18:12:16 +0900

    refactor: setting 도메인 flyway 적용

---
+++ backend/src/main/resources/db/migration/mysql/V1.3.0__setting.sql
@@ -0,0 +1,12 @@
+DROP TABLE settings_dependent_settings;
+DROP TABLE setting;
+
+create table setting (
+                         is_active boolean not null,
+                         id bigint auto_increment,
+                         member_id bigint,
+                         setting_type varchar(255) check (setting_type in ('NOTIFICATION_PER_THUMBS','NOTIFICATION_PER_10_THUMBS','NOTIFICATION_PER_COMMENT','NOTIFICATION_CONSUMPTION_CONFIRMATION_REMINDING','NOTIFICATION')),
+                         primary key (id)
+);
+
+alter table setting add foreign key (member_id) references member (id);
