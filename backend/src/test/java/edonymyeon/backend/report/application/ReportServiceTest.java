480b9a21df8a23a259f0f788d8b7b60aedf9163f
김지현 <pig9619@naver.com>
2023-09-07 22:05:26 +0900

    Feat/#387 댓글 신고기능 (#390)
    * feat: 신고 api를 수정하고, 댓글도 신고할 수 있도록 한다
    
    * feat: 로컬에서 flyway가 충돌나는 것을 막기 위한 bean을 등록한다.
    
    * feat: 바뀐 신고 도메인에 대해 flyway update 문서를 작성한다
    
    * test: 댓글 신고 테스트 케이스 추가
    
    * refactor: flyway 설정 파일 삭제
    
    * feat: reporter lazy loading으로 수정
    
    * refactor: request에서 reportType을 문자열로 받아오고 service에서 타입을 찾아오도록 수정함
    
    * refactor: flyway 버전 수정
    
    * refactor: 바뀐 코드에 맞게 테스트와 로직 수정

---
+++ backend/src/test/java/edonymyeon/backend/report/application/ReportServiceTest.java
@@ -0,0 +1,82 @@
+package edonymyeon.backend.report.application;
+
+import edonymyeon.backend.comment.domain.Comment;
+import edonymyeon.backend.global.exception.EdonymyeonException;
+import edonymyeon.backend.member.application.dto.ActiveMemberId;
+import edonymyeon.backend.post.domain.Post;
+import edonymyeon.backend.report.domain.Report;
+import edonymyeon.backend.support.CommentTestSupport;
+import edonymyeon.backend.support.IntegrationTest;
+import edonymyeon.backend.support.PostTestSupport;
+import edonymyeon.backend.support.TestMemberBuilder;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import static edonymyeon.backend.global.exception.ExceptionInformation.COMMENT_ID_NOT_FOUND;
+import static edonymyeon.backend.global.exception.ExceptionInformation.POST_ID_NOT_FOUND;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SuppressWarnings("NonAsciiCharacters")
+@RequiredArgsConstructor
+@IntegrationTest
+class ReportServiceTest {
+
+    private final ReportService reportService;
+
+    private final ReportRepository reportRepository;
+
+    private final PostTestSupport postTestSupport;
+
+    private final CommentTestSupport commentTestSupport;
+
+    private final TestMemberBuilder memberTestSupport;
+
+    @Test
+    void 특정_게시글을_신고할_수_있다() {
+        final Post post = postTestSupport.builder().build();
+        final Long reportId = reportService
+                .report(
+                        new ReportRequest("POST", post.getId(), 4, null),
+                        new ActiveMemberId(memberTestSupport.builder().build().getId())
+                );
+
+        final Report report = reportRepository.findById(reportId).get();
+        assertThat(report).extracting("id").isEqualTo(reportId);
+    }
+
+    @Test
+    void 존재하지_않는_게시글은_신고할_수_없다() {
+        assertThatThrownBy(() -> reportService
+                .report(
+                        new ReportRequest("POST", -2L, 4, ""),
+                        new ActiveMemberId(memberTestSupport.builder().build().getId()))
+        )
+                .isInstanceOf(EdonymyeonException.class)
+                .hasMessage(POST_ID_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    void 특정_댓글을_신고할_수_있다() {
+        final Comment comment = commentTestSupport.builder().build();
+        final Long reportId = reportService
+                .report(
+                        new ReportRequest("COMMENT", comment.getId(), 4, null),
+                        new ActiveMemberId(memberTestSupport.builder().build().getId())
+                );
+
+        final Report report = reportRepository.findById(reportId).get();
+        assertThat(report).extracting("id").isEqualTo(reportId);
+    }
+
+    @Test
+    void 존재하지_않는_댓글은_신고할_수_없다() {
+        assertThatThrownBy(() -> reportService
+                .report(
+                        new ReportRequest("COMMENT", -2L, 4, ""),
+                        new ActiveMemberId(memberTestSupport.builder().build().getId()))
+        )
+                .isInstanceOf(EdonymyeonException.class)
+                .hasMessage(COMMENT_ID_NOT_FOUND.getMessage());
+    }
+}
