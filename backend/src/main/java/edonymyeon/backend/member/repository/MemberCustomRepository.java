941c1948218103d15b192532facbbf527271d047
BackFoxx <backfoxxx@gmail.com>
2023-09-10 11:27:59 +0900

    feat: 소비/절약 확정을 하지 않은 게시글의 작성자를 전부 찾아내는 Repository 기능 구현

---
+++ backend/src/main/java/edonymyeon/backend/member/repository/MemberCustomRepository.java
@@ -0,0 +1,9 @@
+package edonymyeon.backend.member.repository;
+
+import edonymyeon.backend.member.domain.Member;
+import java.util.List;
+
+public interface MemberCustomRepository {
+
+    List<Member> findAllHavingUnConfirmedPost();
+}
