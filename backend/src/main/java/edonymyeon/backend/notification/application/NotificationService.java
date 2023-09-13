56be640910adb3592b97cd568ad5fc3d200005aa
BackFoxx <backfoxxx@gmail.com>
2023-09-13 10:56:52 +0900

    docs: notificationService의 각 작업 내용을 javadocs로 설명

--- backend/src/main/java/edonymyeon/backend/notification/application/NotificationService.java
+++ backend/src/main/java/edonymyeon/backend/notification/application/NotificationService.java
@@ -51,6 +51,12 @@ public class NotificationService {
 
     private final ThumbsService thumbsService;
 
+    /**
+     * 특정 회원이 받은 알림 내역을 조회합니다.
+     * @param memberId 알림 내역을 조회할 회원의 식별자
+     * @param findingCondition 페이징 조건
+     * @return 페이징 처리된 알림 내역
+     */
     public PostSlice<NotificationResponse> findNotifications(MemberId memberId,
                                                              final GeneralFindingCondition findingCondition) {
         final Slice<Notification> notificationSlice = notificationRepository.findByMemberId(memberId.id(), findingCondition.toPage());
@@ -59,6 +65,10 @@ public class NotificationService {
         return PostSlice.from(notificationResponses);
     }
 
+    /**
+     * 자신의 게시글에 누군가 추천/비추천을 남겼음을 알리는 알림을 발송합니다
+     * @param post 추천/비추천이 달린 게시글
+     */
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void sendThumbsNotificationToWriter(final Post post) {
         final Optional<String> deviceToken = post.getMember().getActiveDeviceToken();
@@ -81,6 +91,10 @@ public class NotificationService {
         return reactionCount % 10 == 0;
     }
 
+    /**
+     * 자신의 게시글에 댓글이 달렸음을 알리는 알림을 발송합니다.
+     * @param comment 게시글에 달린 댓글
+     */
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void sendCommentNotificationToPostWriter(Comment comment) {
         if (settingService.isSettingActive(new ActiveMemberId(comment.getPost().getWriterId()), SettingType.NOTIFICATION_PER_COMMENT)) {
@@ -95,6 +109,9 @@ public class NotificationService {
         }
     }
 
+    /**
+     * 소비/절약 확정을 하지 않은 게시글 작성자들에게 오후 8시마다 리마인드 알림을 발송합니다.
+     */
     // 매일 오후 8시
     @Scheduled(cron = "0 0 20 * * *")
     @Transactional(readOnly = true)
@@ -113,12 +130,19 @@ public class NotificationService {
         }
     }
 
+    /**
+     * 사용자에게 알림을 전송합니다.
+     * @param notifyingTarget 알림을 전송할 대상
+     * @param notifyingType 알림을 클릭했을 때 리다이렉트할 페이지의 종류
+     * @param redirectId 알림을 클릭했을 때 리다이렉트할 페이지의 id
+     * @param notificationMessage 알림에서 표시할 제목
+     */
     private void sendNotification(final Member notifyingTarget, final ScreenType notifyingType, final Long redirectId,
                                   final NotificationMessage notificationMessage) {
         final Long notificationId = saveNotification(notifyingTarget, notifyingType, redirectId, notificationMessage);
 
         if (notifyingTarget.isDeleted()) {
-            log.info("탈퇴한 회원에 대한 알림 전송 시도입니다.");
+            log.warn("탈퇴한 회원에 대한 알림 전송 시도입니다.");
             return;
         }
 
@@ -134,6 +158,14 @@ public class NotificationService {
         }
     }
 
+    /**
+     * 알림 전송 전/후 해당 내용을 저장합니다.
+     * @param notifyingTarget 알림을 전송받은 대상
+     * @param notifyingType 알림을 클릭했을 때 리다이렉트한 페이지의 종류
+     * @param redirectId 알림을 클릭했을 때 리다이렉트한 페이지의 id
+     * @param notificationMessage 알림에서 표시한 제목
+     * @return 알림 식별자
+     */
     private Long saveNotification(final Member notifyingTarget, final ScreenType notifyingType, final Long redirectId,
                           final NotificationMessage notificationMessage) {
         final Notification notification = new Notification(
@@ -145,12 +177,20 @@ public class NotificationService {
         return notificationRepository.save(notification).getId();
     }
 
+    /**
+     * 알림을 '읽었음' 표시합니다.
+     * @param notificationId '읽었음' 표시할 알림의 식별자
+     */
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void markNotificationAsRead(final Long notificationId) {
         final Optional<Notification> notification = notificationRepository.findById(notificationId);
         notification.ifPresent(Notification::markAsRead);
     }
 
+    /**
+     * 특정 게시글로부터 발생한 알림 내역을 전부 삭제합니다.
+     * @param postId 게시글의 식별자
+     */
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void deleteNotificationByPost(final Long postId) {
         notificationRepository.deleteAllByPostId(postId);
