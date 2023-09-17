de19738e6be6f1657101f423ab7ca815fadb1eeb
This2sho <zx00018@naver.com>
2023-09-17 18:25:23 +0900

    test: 로그아웃(세션 만료) 이후, 기존 session으로 로그인시 예외처리 확인하는 테스트 코드 작성

--- backend/src/test/java/edonymyeon/backend/auth/AuthIntegrationTest.java
+++ backend/src/test/java/edonymyeon/backend/auth/AuthIntegrationTest.java
@@ -1,5 +1,6 @@
 package edonymyeon.backend.auth;
 
+import static edonymyeon.backend.global.exception.ExceptionInformation.AUTHORIZATION_EMPTY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.mockito.ArgumentMatchers.any;
@@ -12,12 +13,17 @@ import edonymyeon.backend.auth.application.dto.JoinRequest;
 import edonymyeon.backend.auth.application.dto.KakaoLoginRequest;
 import edonymyeon.backend.auth.application.dto.KakaoLoginResponse;
 import edonymyeon.backend.auth.application.dto.LoginRequest;
+import edonymyeon.backend.auth.application.dto.LogoutRequest;
 import edonymyeon.backend.member.domain.Member;
 import edonymyeon.backend.member.domain.SocialInfo;
 import edonymyeon.backend.member.domain.SocialInfo.SocialType;
 import edonymyeon.backend.member.repository.MemberRepository;
 import edonymyeon.backend.support.IntegrationFixture;
+import edonymyeon.backend.support.TestMemberBuilder;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.Optional;
@@ -250,5 +256,65 @@ class AuthIntegrationTest extends IntegrationFixture {
             softly.assertThat(카카오_로그인전_회원).isEmpty();
             softly.assertThat(카카오_로그인후_회원).isPresent();
         });
+    }
+
+    @Test
+    void 세션만료된_세션정보로_회원_정보_조회시_예외처리() {
+        final Member member = memberTestSupport.builder().build();
+
+        final ExtractableResponse<Response> 로그인_결과 = 로그인_요청(member);
+
+        final String cookieValue = 로그인_결과.header(HttpHeaders.SET_COOKIE);
+        final Header header = new Header(HttpHeaders.COOKIE, cookieValue);
+
+        로그아웃_요청(member, header);
+
+        final ExtractableResponse<Response> 내가_쓴글_조회 = RestAssured
+                .given()
+                .header(header)
+                .when()
+                .get("/profile/my-posts")
+                .then()
+                .extract();
+
+        final JsonPath errorResponse = 내가_쓴글_조회.body().jsonPath();
+        assertSoftly(
+                softly -> {
+                    softly.assertThat(내가_쓴글_조회.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+                    softly.assertThat(errorResponse.getInt("errorCode")).isEqualTo(AUTHORIZATION_EMPTY.getCode());
+                    softly.assertThat(errorResponse.getString("errorMessage"))
+                            .isEqualTo(AUTHORIZATION_EMPTY.getMessage());
+                }
+        );
+    }
+
+    private ExtractableResponse<Response> 로그인_요청(Member member) {
+        final String deviceToken = member.getDevices().get(0).getDeviceToken();
+        final LoginRequest loginRequest = new LoginRequest(member.getEmail(), TestMemberBuilder.getRawPassword(),
+                deviceToken);
+
+        return RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body(loginRequest)
+                .when()
+                .post("/login")
+                .then()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> 로그아웃_요청(Member member, Header header) {
+        final String deviceToken = member.getDevices().get(0).getDeviceToken();
+        final LogoutRequest logoutRequest = new LogoutRequest(deviceToken);
+
+        return RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .header(header)
+                .body(logoutRequest)
+                .when()
+                .post("/logout")
+                .then()
+                .extract();
     }
 }
