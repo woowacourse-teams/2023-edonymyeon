556ccbf12c7f1886df56cbb237ab7736dae3daf4
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-09-18 13:53:50 +0900

    feat: 사진 확대 뷰 구현

---
+++ android/app/src/main/java/com/app/edonymyeon/presentation/common/activity/PictureActivity.kt
@@ -0,0 +1,12 @@
+package com.app.edonymyeon.presentation.common.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.edonymyeon.R
+
+class PictureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture)
+    }
+}
