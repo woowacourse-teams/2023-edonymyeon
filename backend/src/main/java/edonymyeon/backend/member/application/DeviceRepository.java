2ab60d26291479d315f6ec07d576f2bbc65e93eb
BackFoxx <backfoxxx@gmail.com>
2023-10-03 00:08:12 +0900

    feat: findAllByDeviceToken의 N+1발생 문제를 해결

--- backend/src/main/java/edonymyeon/backend/member/application/DeviceRepository.java
+++ backend/src/main/java/edonymyeon/backend/member/application/DeviceRepository.java
@@ -3,6 +3,7 @@ package edonymyeon.backend.member.application;
 import edonymyeon.backend.member.domain.Device;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface DeviceRepository extends JpaRepository<Device, Long> {
     Optional<Device> findByDeviceToken(String deviceToken);
 
+    @EntityGraph(attributePaths = "member")
     List<Device> findAllByDeviceToken(String deviceToken);
 
     Optional<Device> findByDeviceTokenAndIsActiveIsTrue(String deviceToken);
