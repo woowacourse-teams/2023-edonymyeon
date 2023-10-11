4fd68ae6baaffb6a1b0ab80fcf81b7a9867f1943
Sujin Kim <sujinkim919@gmail.com>
2023-10-11 16:08:55 +0900

    Feat/#468 회원정보수정 뷰 구현 (#477)
    * feat: 회원정보수정 페이지 뷰 구현
    
    * feat: BaseActivity, BaseViewModel 적용
    
    * feat: 회원정보수정 뷰 - 수정하기 버튼 추가
    
    * refactor: 필요없는 로그 및 주석 삭제
    
    * feat: 회원정보 수정 시 기존 닉네임 및 이미지가 보이도록 구현
    
    * feat: 갤러리에서 사진 선택 구현
    
    - PickVisualMedia 이용
    - 별도의 권한을 요구하지 않음
    - SDK 버전이 30 미만이거나, SDK
    Extensions의 버전이 2미만일 경우 ACTION_OPEN_DOCUMENT가 실행됨
    
    * feat: 사진 기본 이미지로 변경 구현
    
    * feat: hilt 적용
    
    * feat： 사진 선택 모듈 자동 설치 트리거 추가
    
    * refactor: 필요 없는 코드 제거

---
+++ android/app/src/main/java/com/app/edonymyeon/presentation/ui/profileupdate/ProfileUpdateActivity.kt
@@ -0,0 +1,74 @@
+package com.app.edonymyeon.presentation.ui.profileupdate
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import app.edonymyeon.databinding.ActivityProfileUpdateBinding
+import com.app.edonymyeon.presentation.common.activity.BaseActivity
+import com.app.edonymyeon.presentation.uimodel.WriterUiModel
+import com.app.edonymyeon.presentation.util.getParcelableExtraCompat
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileUpdateActivity : BaseActivity<ActivityProfileUpdateBinding, ProfileUpdateViewModel>(
+    { ActivityProfileUpdateBinding.inflate(it) },
+) {
+    override val viewModel: ProfileUpdateViewModel by viewModels()
+    override val inflater: LayoutInflater by lazy { LayoutInflater.from(this) }
+
+    private val originalProfile by lazy {
+        intent.getParcelableExtraCompat(KEY_PROFILE) as? WriterUiModel
+            ?: throw IllegalArgumentException()
+    }
+
+    private val pickMultipleImage =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                viewModel.setNewProfileImage(uri.toString())
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        initBinding()
+        setOriginalProfile()
+        setImageButtonsClickListener()
+    }
+
+    private fun initBinding() {
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+    }
+
+    private fun setOriginalProfile() {
+        viewModel.initOriginalProfile(originalProfile)
+    }
+
+    private fun setImageButtonsClickListener() {
+        binding.btnImageUpload.setOnClickListener {
+            navigateToGallery()
+        }
+        binding.btnImageDelete.setOnClickListener {
+            viewModel.deleteProfileImage()
+        }
+    }
+
+    private fun navigateToGallery() {
+        pickMultipleImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    companion object {
+        private const val KEY_PROFILE = "key_profile"
+
+        fun newIntent(context: Context, profile: WriterUiModel): Intent {
+            return Intent(context, ProfileUpdateActivity::class.java).apply {
+                putExtra(KEY_PROFILE, profile)
+            }
+        }
+    }
+}
