c8cfe133b3bb34308eeb489e321d9d6c4b4e655e
김지현 <pig9619@naver.com>
2023-10-15 20:57:07 +0900

    refactor: 레디스를 사용하던 핫게시글 캐싱을 맵으로 전환함 (#434)
    * refactor: 레디스를 사용하던 핫게시글 캐싱을 맵으로 전환함
    
    * refactor: repository를 분리함
    
    * refactor: 공유되는 리스트를 수정 불가능하게 수정

---
+++ backend/src/main/java/edonymyeon/backend/cache/repository/HotPostsRepository.java
@@ -0,0 +1,34 @@
+package edonymyeon.backend.cache.repository;
+
+import edonymyeon.backend.cache.domain.CachedHotPost;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class HotPostsRepository {
+
+    private HotPostsRepository() {
+    }
+
+    private static final Map<String, CachedHotPost> cacheStorage = new ConcurrentHashMap<>();
+
+    public static boolean hasKey(final String key) {
+        return cacheStorage.containsKey(key);
+    }
+
+    public static Optional<CachedHotPost> get(final String key) {
+        if (hasKey(key)) {
+            return Optional.of(cacheStorage.get(key));
+        }
+        return Optional.empty();
+    }
+
+    public static void save(final String key, final CachedHotPost cachedHotPost) {
+        cacheStorage.put(key, cachedHotPost);
+    }
+
+    public static void delete(final String key) {
+        cacheStorage.remove(key);
+    }
+}
