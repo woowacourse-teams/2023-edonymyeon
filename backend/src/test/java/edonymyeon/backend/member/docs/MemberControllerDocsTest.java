fc430c84ca8a5e0a8f8e416c2106418be0280c47
This2sho <zx00018@naver.com>
2023-10-17 17:04:03 +0900

    docs: rest docs id 변경

--- backend/src/test/java/edonymyeon/backend/member/docs/MemberControllerDocsTest.java
+++ backend/src/test/java/edonymyeon/backend/member/docs/MemberControllerDocsTest.java
@@ -219,7 +219,7 @@ class MemberControllerDocsTest extends DocsTest {
                 .header("X-API-VERSION", 1)
                 .sessionAttr(USER.getSessionId(), 회원.getId());
 
-        final RestDocumentationResultHandler 문서화 = document("profile",
+        final RestDocumentationResultHandler 문서화 = document("profile-v1",
                 responseFields(fieldWithPath("id").description("회원 id"),
                         fieldWithPath("nickname").description("닉네임")));
 
@@ -239,7 +239,7 @@ class MemberControllerDocsTest extends DocsTest {
                 .header("X-API-VERSION", 2)
                 .sessionAttr(USER.getSessionId(), 회원.getId());
 
-        final RestDocumentationResultHandler 문서화 = document("profile",
+        final RestDocumentationResultHandler 문서화 = document("profile-v2",
                 responseFields(fieldWithPath("id").description("회원 id"),
                         fieldWithPath("nickname").description("닉네임"),
                         fieldWithPath("profileImage").description("프로필 사진")));
