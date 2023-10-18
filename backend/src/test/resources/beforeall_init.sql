214c4445033f266e50539d11244f94392b6b5a93
BackFoxx <backfoxxx@gmail.com>
2023-10-19 06:07:10 +0900

    fix: 테스트 깨짐 방지를 위해 기본 알림 메시지를 저장

---
+++ backend/src/test/resources/beforeall_init.sql
@@ -0,0 +1,11 @@
+INSERT INTO notification_content (id, title, body)
+values ('THUMBS_NOTIFICATION_TITLE', '개별 따봉 알림입니다', '개별 따봉 알림 본문입니다');
+
+INSERT INTO notification_content (id, title, body)
+values ('THUMBS_PER_10_NOTIFICATION_TITLE', '10건 따봉 알림입니다', '10건 따봉 알림 본문입니다');
+
+INSERT INTO notification_content (id, title, body)
+values ('COMMENT_NOTIFICATION_TITLE', '댓글 알림입니다', '댓글 알림 본문입니다');
+
+INSERT INTO notification_content (id, title, body)
+values ('UNCONFIRMED_POST_REMINDER_TITLE', '리마인드 알림입니다', '리마인드 알림 본문입니다');
