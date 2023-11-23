0c39ba3599e4694f17206aed79163166d1e263e1
BackFoxx <backfoxxx@gmail.com>
2023-11-23 22:40:20 +0900

    refactor : 알림을 전송하는 sendNotification을 오버로딩하여 early return 작업과 실제 알림 전송 작업을 분리

--- backend/src/main/java/edonymyeon/backend/notification/application/NotificationService.java
+++ backend/src/main/java/edonymyeon/backend/notification/application/NotificationService.java
@@ -1,9 +1,5 @@
 package edonymyeon.backend.notification.application;
 
-import static edonymyeon.backend.notification.domain.NotificationMessage.COMMENT_NOTIFICATION_TITLE;
-import static edonymyeon.backend.notification.domain.NotificationMessage.THUMBS_NOTIFICATION_TITLE;
-import static edonymyeon.backend.notification.domain.NotificationMessage.THUMBS_PER_10_NOTIFICATION_TITLE;
-
 import edonymyeon.backend.comment.domain.Comment;
 import edonymyeon.backend.consumption.application.ConsumptionService;
 import edonymyeon.backend.global.exception.BusinessLogicException;
@@ -25,16 +21,18 @@ import edonymyeon.backend.setting.application.SettingService;
 import edonymyeon.backend.setting.domain.SettingType;
 import edonymyeon.backend.thumbs.application.ThumbsService;
 import jakarta.persistence.EntityManager;
-import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Slice;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static edonymyeon.backend.notification.domain.NotificationMessage.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -57,7 +55,8 @@ public class NotificationService {
 
     /**
      * 특정 회원이 받은 알림 내역을 조회합니다.
-     * @param memberId 알림 내역을 조회할 회원의 식별자
+     *
+     * @param memberId         알림 내역을 조회할 회원의 식별자
      * @param findingCondition 페이징 조건
      * @return 페이징 처리된 알림 내역
      */
@@ -71,6 +70,7 @@ public class NotificationService {
 
     /**
      * 자신의 게시글에 누군가 추천/비추천을 남겼음을 알리는 알림을 발송합니다
+     *
      * @param post 추천/비추천이 달린 게시글
      */
     @Async
@@ -80,14 +80,15 @@ public class NotificationService {
             return;
         }
 
+        final Member writer = post.getMember();
         if (settingService.isSettingActive(new ActiveMemberId(post.getWriterId()), SettingType.NOTIFICATION_PER_10_THUMBS)
                 && isDivisibleBy10(thumbsService.countReactions(post.getId()))) {
-            sendNotification(post.getMember(), ScreenType.POST, post.getId(), THUMBS_PER_10_NOTIFICATION_TITLE);
+            sendNotification(writer, ScreenType.POST, post.getId(), THUMBS_PER_10_NOTIFICATION_TITLE);
             return;
         }
 
         if (settingService.isSettingActive(new ActiveMemberId(post.getWriterId()), SettingType.NOTIFICATION_PER_THUMBS)) {
-            sendNotification(post.getMember(), ScreenType.POST, post.getId(), THUMBS_NOTIFICATION_TITLE);
+            sendNotification(writer, ScreenType.POST, post.getId(), THUMBS_NOTIFICATION_TITLE);
         }
     }
 
@@ -97,6 +98,7 @@ public class NotificationService {
 
     /**
      * 자신의 게시글에 댓글이 달렸음을 알리는 알림을 발송합니다.
+     *
      * @param comment 게시글에 달린 댓글
      */
     @Async
@@ -106,10 +108,11 @@ public class NotificationService {
             return;
         }
 
+        final Member writer = comment.getPostWriter();
+
         if (settingService.isSettingActive(new ActiveMemberId(comment.getPost().getWriterId()), SettingType.NOTIFICATION_PER_COMMENT)) {
             comment = entityManager.merge(comment);
-
-            sendNotification(comment.getPostWriter(), ScreenType.POST, comment.findPostId(), COMMENT_NOTIFICATION_TITLE);
+            sendNotification(writer, ScreenType.POST, comment.findPostId(), COMMENT_NOTIFICATION_TITLE);
         }
     }
 
@@ -135,46 +138,75 @@ public class NotificationService {
     }
 
     /**
-     * 사용자에게 알림을 전송합니다.
-     * @param member 알림을 전송할 대상
-     * @param notifyingType 알림을 클릭했을 때 리다이렉트할 페이지의 종류
-     * @param redirectId 알림을 클릭했을 때 리다이렉트할 페이지의 id
+     * @param member              알림을 전송할 회원
+     * @param notifyingType       알림을 클릭했을 때 리다이렉트할 페이지의 종류
+     * @param redirectId          알림을 클릭했을 때 리다이렉트할 페이지의 id
      * @param notificationMessage 알림에서 표시할 제목
      */
-    private void sendNotification(final Member member, final ScreenType notifyingType, final Long redirectId,
-                                  final NotificationMessage notificationMessage) {
+    private void sendNotification(
+            final Member member,
+            final ScreenType notifyingType,
+            final Long redirectId,
+            final NotificationMessage notificationMessage
+    ) {
         if (member.isDeleted()) {
             return;
         }
 
-        final Long notificationId = saveNotification(member.getId(), notifyingType, redirectId, notificationMessage);
+        Long notificationId = saveNotification(member.getId(), notifyingType, redirectId, notificationMessage);
 
-        if (member.hasActiveDeviceToken()) {
-            final Receiver receiver = new Receiver(
-                    member.getActiveDeviceToken(),
-                    new Data(notificationId, notifyingType, redirectId)
+        if (!member.hasActiveDeviceToken()) {
+            return;
+        }
+
+        sendNotification(notificationId, member.getActiveDeviceToken(), notifyingType, redirectId, notificationMessage);
+    }
+
+    /**
+     * 사용자에게 알림을 전송합니다.
+     *
+     * @param notificationId      전송할 알림의 식별자
+     * @param deviceToken         알림을 전송할 디바이스 토큰
+     * @param notifyingType       알림을 클릭했을 때 리다이렉트할 페이지의 종류
+     * @param redirectId          알림을 클릭했을 때 리다이렉트할 페이지의 id
+     * @param notificationMessage 알림에서 표시할 제목
+     */
+    private void sendNotification(
+            final Long notificationId,
+            final String deviceToken,
+            final ScreenType notifyingType,
+            final Long redirectId,
+            final NotificationMessage notificationMessage
+    ) {
+        final Receiver receiver = new Receiver(
+                deviceToken,
+                new Data(notificationId, notifyingType, redirectId)
+        );
+        try {
+            notificationSender.sendNotification(
+                    receiver,
+                    notificationMessage.getMessage()
             );
-            try {
-                notificationSender.sendNotification(
-                        receiver,
-                        notificationMessage.getMessage()
-                );
-            } catch (BusinessLogicException e) {
-                log.error("알림 전송에 실패했습니다.", e);
-            }
+        } catch (BusinessLogicException e) {
+            log.error("알림 전송에 실패했습니다.", e);
         }
     }
 
     /**
      * 알림 전송 전/후 해당 내용을 저장합니다.
-     * @param memberId 알림을 전송받은 대상의 식별자
-     * @param notifyingType 알림을 클릭했을 때 리다이렉트한 페이지의 종류
-     * @param redirectId 알림을 클릭했을 때 리다이렉트한 페이지의 id
+     *
+     * @param memberId            알림을 전송받은 대상의 식별자
+     * @param notifyingType       알림을 클릭했을 때 리다이렉트한 페이지의 종류
+     * @param redirectId          알림을 클릭했을 때 리다이렉트한 페이지의 id
      * @param notificationMessage 알림에서 표시한 제목
      * @return 알림 식별자
      */
-    private Long saveNotification(final Long memberId, final ScreenType notifyingType, final Long redirectId,
-                          final NotificationMessage notificationMessage) {
+    private Long saveNotification(
+            final Long memberId,
+            final ScreenType notifyingType,
+            final Long redirectId,
+            final NotificationMessage notificationMessage
+    ) {
         final Notification notification = new Notification(
                 memberId,
                 notificationMessage.getMessage(),
@@ -186,6 +218,7 @@ public class NotificationService {
 
     /**
      * 알림을 '읽었음' 표시합니다.
+     *
      * @param notificationId '읽었음' 표시할 알림의 식별자
      */
     @Async
@@ -197,6 +230,7 @@ public class NotificationService {
 
     /**
      * 특정 게시글로부터 발생한 알림 내역을 전부 삭제합니다.
+     *
      * @param postId 게시글의 식별자
      */
     @Async
